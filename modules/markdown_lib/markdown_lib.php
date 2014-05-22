a624b48506a489aeb785ffb03ccf3a73bad71585
Daniel Pimley <dan@pimley.net>
2014-05-22 13:37:04 +0100

    Fixed some bugs

--- modules/markdown_lib/markdown_lib.php
+++ modules/markdown_lib/markdown_lib.php
@@ -3,7 +3,7 @@
     require_once "lib/Markdown.php";
     require_once "lib/MarkdownExtra.php";
 
-    class MarkdownLibrary extends Modules {
+    class MarkdownLib extends Modules {
         public function __init() {
             $this->addAlias("markup_text", "markdownify", 8);
             $this->addAlias("preview", "markdownify", 8);
