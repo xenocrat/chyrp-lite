7eed9f211fc82d45b025e6ffaca8b7f783771a3f
Daniel Pimley <dan@pimley.net>
2014-05-25 14:41:07 +0100

    Blog theme: JS-assisted mobile menu
    Added a very lightweight script to add an expandable “cheeseburger”
    menu for mobile screen sizes. Adapts the static mobile footer menu.

--- themes/blossom/javascripts/theme.js.php
+++ themes/blossom/javascripts/theme.js.php
@@ -12,4 +12,28 @@ function mailTo(domain, recipient) {
     return true;
 }
 
+// Cheeseburger mobile menu
+$(function(){
+    $('<li>', {
+            "id": "mobile_toggle",
+            "role": "button",
+            "aria-label": "Menu",
+        }).css({
+            "cursor": "pointer"
+        }).click( function() {
+            if ( $(this).parent().hasClass("on") ) {
+                $(this).parent().removeClass("on");
+            } else {
+                $(this).parent().addClass("on");
+            }
+            return false;
+        }).append($("<img>", {
+            "id": "cheeseburger",
+            "src": "<?php echo ( THEME_URL."/images/cheeseburger.svg") ?>",
+            "alt": "Menu"
+        })).appendTo("ul.tail_nav").parent().addClass("mobile_nav").parents("body").css({
+            "margin-bottom": "3em"
+        });
+})
+
 <!-- --></script>
