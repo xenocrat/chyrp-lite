c16b0ae8afb179f7318f80dbc0d3a24897ec769f
Daniel Pimley <dan@pimley.net>
2015-04-16 22:22:19 +0100

    Blog themes: minor changes

--- themes/topaz/info.php
+++ themes/topaz/info.php
@@ -3,7 +3,7 @@ return array(
     "name"          => __("Topaz", "topaz"),
     "url"           => "http://chyrplite.net/",
     "version"       => 1.0,
-    "description"   => __("A minimalist responsive theme for Chyrp with emphasis on clean presentation of images and text.", "topaz"),
+    "description"   => __("A minimalist responsive theme for Chyrp Lite.", "topaz"),
     "author"        => array(
         "name"      => "Daniel Pimley",
         "url"       => "http://pimley.net/")
