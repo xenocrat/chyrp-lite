5ee87a4fd71d36ea9a868b5d92698efce1c51a9a
Daniel Pimley <dan@pimley.net>
2015-04-18 00:31:46 +0100

    Sparrow: new tumbleblog theme

---
+++ themes/sparrow/info.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    "name"        	=> __("Sparrow", "sparrow"),
+    "url"         	=> "http://chyrplite.net/",
+    "version"     	=> 1.0,
+    "description" 	=> __("An unobtrusive tumbleblog theme for Chyrp Lite.", "sparrow"),
+    "author"      	=> array(
+        "name"    	=> "Daniel Pimley",
+        "url"     	=> "http://pimley.net/")
+);
