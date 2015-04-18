1290cea230e631d77b3f7ca77a1e50c232c180c6
Daniel Pimley <dan@pimley.net>
2015-04-18 19:33:59 +0100

    Highlighter module: updated metadata

--- modules/highlighter/info.php
+++ modules/highlighter/info.php
@@ -3,7 +3,7 @@ return array(
     "name"          => __("highlighter.js", "highlighter"),
     "url"           => "http://chyrplite.net/",
     "version"       => 1.0,
-    "description"   => __("Adds syntax highlighting courtesy of <a href='https://highlightjs.org/'>highlight.js</a>.", "highlighter"),
+    "description"   => __("Adds syntax highlighting to nested <code>&lt;pre&gt;&lt;code&gt;</code> blocks, courtesy of <a href='https://highlightjs.org/'>highlight.js</a>.", "highlighter"),
     "author"        => array(
         "name"      => "Daniel Pimley",
         "url"       => "http://www.pimley.net/")
