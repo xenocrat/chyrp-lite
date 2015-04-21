c62215a95b1da6b3e7804f8d2167bedaa5c7c26e
Daniel Pimley <dan@pimley.net>
2015-04-21 23:06:16 +0100

    Giving gravatar images a class name

--- includes/helpers.php
+++ includes/helpers.php
@@ -1817,10 +1817,10 @@
     function get_gravatar($email, $s = 80, $d = "mm", $r = "g", $img = false, $atts = array()) {
     	$url = "http://www.gravatar.com/avatar/".md5(strtolower(trim($email)))."?s=$s&d=$d&r=$r";
     	if ($img) {
-    		$url = '<img src="' . $url . '"';
+    		$url = '<img class="gravatar" src="' . $url . '"';
     		foreach ($atts as $key => $val)
     			$url .= ' ' . $key . '="' . $val . '"';
-    		$url .= " />";
+    		$url .= ">";
     	}
     	return $url;
     }
