449c4e403abbc8f3a5da13bd7dc8933e03b913ed
Daniel Pimley <dan@pimley.net>
2015-04-22 18:43:49 +0100

    Error.php: making meta tag HTML5 style

--- includes/error.php
+++ includes/error.php
@@ -20,7 +20,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <title><?php echo $title; ?></title>
         <meta name="viewport" content="width = 520, user-scalable = no">
         <style type="text/css">
