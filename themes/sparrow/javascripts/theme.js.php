657f1afc2044757988d2f78326a0f685a5dc75b8
Daniel Pimley <dan@pimley.net>
2015-09-26 16:16:44 +0100

    Tidying comments

--- themes/sparrow/javascripts/theme.js.php
+++ themes/sparrow/javascripts/theme.js.php
@@ -6,13 +6,13 @@
 ?>
 <!-- --><script>
 
-// Obfuscated mailto
+// Obfuscated mailto.
 function mailTo(domain, recipient) {
     location.assign(('mailto:' + recipient + '@' + domain));
     return true;
 }
 
-// Prevent tabbing through yearly archive post previews
+// Prevent tabbing through yearly archive post previews.
 $(document).ready(function() {
 	$("article.post.archive *").not(".archive_post_link").attr("tabindex", "-1")
 });
