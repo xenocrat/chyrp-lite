4aeedfad2c3da02795ff58281cccb3dba06b6ca3
Daniel Pimley <dan@pimley.net>
2015-11-09 20:17:46 +0000

    Sitemap Module: removed prefixed namespace definitions

--- modules/sitemap/sitemap.php
+++ modules/sitemap/sitemap.php
@@ -87,8 +87,7 @@
             $sitemap_settings = Config::current()->module_sitemap;
 
             $output = "<?xml version='1.0' encoding='UTF-8'?>".PHP_EOL;
-            $output.= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'.
-                      ' xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">'."\n";
+            $output.= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'."\n";
 
             $output.= "  <url>\n".
                       "    <loc>$config->url/</loc>\n".
