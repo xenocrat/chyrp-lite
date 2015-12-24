c1710cc1ab875d859137dcca3ab7f2ed6d922641
Daniel Pimley <dan@pimley.net>
2015-12-24 14:40:51 +0000

    ReadMore module: refactored and simplified
    Adding “…more” links is now a one-step process without need for a
    read_more filter in the Twig files; custom link text can be specified
    inside the comment <!—more like this—>.

--- modules/read_more/read_more.php
+++ modules/read_more/read_more.php
@@ -1,42 +1,29 @@
 <?php
     class ReadMore extends Modules {
         public function __init() {
-            $this->addAlias("markup_post_text", "makesafe", 4); # Replace "<!--more-->" before markup modules filter it.
+            $this->addAlias("markup_post_text", "more", 4); # Replace "<!--more-->" before markup modules filter it.
         }
 
-        static function makesafe($text, $post = null) {
-            if (!is_string($text) or strpos($text, "<!--more-->") === false)
+        static function more($text, $post = null) {
+            if (!is_string($text) or preg_match("/<!--more(.+?)?-->/i", $text, $matches) === 0)
                 return $text;
 
-            $controller = Route::current()->controller;
-            if ($controller instanceof MainController and $controller->feed)
-                return str_replace("<!--more-->", "", $text);
+            $route = Route::current();
+            $controller = $route->controller;
 
+            if ($route->action == "view" or $route->action == "preview" or
+                ($controller instanceof MainController and $controller->feed))
+                return preg_replace("/<!--more(.+?)?-->/i", "", $text);
+
+            $more = oneof(trim(fallback($matches[1])), __("&hellip;more", "read_more"));
             $url = (isset($post) and !$post->no_results) ? $post->url() : "#" ;
+            $split = preg_split("/<!--more(.+?)?-->/i", $text, -1, PREG_SPLIT_NO_EMPTY);
 
-            return str_replace("<!--more-->", '<a class="read_more" href="'.$url.'">e51b2b9a58824dd068d8777ec6e97e4d</a>', $text);
-        }
-
-        static function read_more($text, $string = null) {
-            if (!substr_count($text, "e51b2b9a58824dd068d8777ec6e97e4d")) # md5 hash of "replace me!"
-                return $text;
-
-            if (Route::current()->action == "view" or (isset($_POST['context']) and $_POST['context'] == "view"))
-                return preg_replace('/(<p>)?<a class="read_more" href="([^"]+)">e51b2b9a58824dd068d8777ec6e97e4d<\/a>(<\/p>)?/', "", $text);
-
-            $plaintext = preg_replace("/<[^>]+>/", "", preg_replace("/&[0-9a-z]{2,8};|&#[0-9]{1,7};|&#x[0-9a-f]{1,6};/i", " ", $text));
-
-            $breakpoint = strpos($plaintext, "e51b2b9a58824dd068d8777ec6e97e4d") + strlen("e51b2b9a58824dd068d8777ec6e97e4d");
-
-            $body = truncate($text, $breakpoint, "", true, true); # Truncation will retain the closing tag of the read_more link.
-
-            return str_replace("e51b2b9a58824dd068d8777ec6e97e4d",
-                               fix((!isset($string) or $string instanceof Post) ? __("&hellip;more", "read_more") : $string),
-                               $body);
+            return $split[0].'<a class="read_more" href="'.$url.'">'.$more.'</a>';
         }
 
         static function title_from_excerpt($text) {
-            $split = preg_split('/(<p>)?<a class="read_more" href="([^"]+)">e51b2b9a58824dd068d8777ec6e97e4d<\/a>(<\/p>)?/', $text);
+            $split = preg_split('/<a class="read_more"/', $text);
             return $split[0];
         }
     }
