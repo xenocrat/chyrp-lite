394a9bf531a8b57884ade8b245df8526284330ed
Daniel Pimley <dan@pimley.net>
2016-01-09 21:59:16 +0000

    Code cleaning

--- includes/javascript.php
+++ includes/javascript.php
@@ -18,8 +18,7 @@ var Route = {
 var Site = {
     url: '<?php echo $config->chyrp_url; ?>',
     key: '<?php if (logged_in() and strpos($_SERVER["HTTP_REFERER"], $config->url) === 0) echo token($_SERVER["REMOTE_ADDR"]); ?>',
-    ajax: <?php echo($config->enable_ajax ? "true" : "false"); ?>
-
+    ajax: <?php echo($config->enable_ajax ? "true" : "false"); ?> 
 }
 var Passwords = {
     check: function(selector_primary, selector_confirm) {
