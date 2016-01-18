2ca8aca723ccd95f1e72361643659446b3ab5f80
Daniel Pimley <dan@pimley.net>
2016-01-18 11:06:21 +0000

    Update class: added experimental auto-update functionality
    Not enabled for this release.

--- includes/class/Update.php
+++ includes/class/Update.php
@@ -20,10 +20,37 @@
 
             foreach ($xml->channel->item as $item) {
                 if (version_compare(CHYRP_VERSION, $item->version, "<")) {
+                    #if (Config::current()->auto_update and class_exists("ZipArchive"))
+                    #    self::install($item->downloadurl);
+
                     Flash::message(_f("Chyrp Lite v%s is available.", $item->version).
                                    ' <a href="'.$item->updateurl.'" target="_blank">'.__("Go to GitHub &rarr;").'</a>');
                     break;
                 }
             }
+        }
+
+        /**
+         * Function: install
+         * Download and install Chyrp Lite updates from the web.
+         */
+        private static function install($url) {
+            if (DEBUG)
+                error_log("INSTALLING UPDATE: ".$url); 
+
+            $filename = upload_from_url($url);
+            $filepath = MAIN_DIR.Config::current()->uploads_path.$filename;
+
+            $zip = new ZipArchive;
+            $err = $zip->open($filepath);
+
+            if ($err === true) {
+                $zip->extractTo(MAIN_DIR);
+                $zip->close();
+                unlink($filepath);
+                redirect("/upgrade.php?upgrade=yes", true);
+                exit;
+            } else
+                error(__("Error"), _f("Failed to install Chyrp Lite update because of ZipArchive error: <code>%s</code>", zip_errors($err)));
         }
     }
