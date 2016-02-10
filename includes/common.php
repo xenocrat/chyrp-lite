85e4584cc57a0cdc36fd5ab99b879ac8189fdddc
Daniel Pimley <dan@pimley.net>
2016-02-10 19:11:22 +0000

    Setting development version and codename

--- includes/common.php
+++ includes/common.php
@@ -5,7 +5,7 @@
      * Chyrp Lite: An ultra-lightweight blogging engine.
      *
      * Version:
-     *     v2016.01
+     *     v2016.02
      *
      * Copyright:
      *     Chyrp Lite is Copyright 2008-2016 Alex Suraci, Arian Xhezairi,
@@ -45,11 +45,11 @@
 
     # Constant: CHYRP_VERSION
     # Version number for this release.
-    define('CHYRP_VERSION', "2016.01");
+    define('CHYRP_VERSION', "2016.02");
 
     # Constant: CHYRP_CODENAME
     # The code name for this version.
-    define('CHYRP_CODENAME', "Socotra");
+    define('CHYRP_CODENAME', "Russet");
 
     # Constant: CACHE_TWIG
     # Override DEBUG to enable Twig template caching.
@@ -350,14 +350,14 @@
     # Initiate the extensions.
     init_extensions();
 
-    # Prepare the trigger class
+    # Prepare the trigger class.
     $trigger = Trigger::current();
 
     # Filter the visitor immediately after the Modules are initialized.
     # Example usage scenario: custom auth systems (e.g. OpenID)
     $trigger->filter($visitor, "visitor");
 
-    # First general-purpose trigger. There are many cases you may want to use @route_init@ instead of this, however.
+    # First general-purpose trigger. There are many cases you may want to use @route_init@ instead of this.
     $trigger->call("runtime");
 
     # Set the content-type and charset.
--- upgrade.php
+++ upgrade.php
@@ -9,8 +9,8 @@
     header("Content-type: text/html; charset=UTF-8");
 
     define('DEBUG',          true);
-    define('CHYRP_VERSION',  "2016.01");
-    define('CHYRP_CODENAME', "Socotra");
+    define('CHYRP_VERSION',  "2016.02");
+    define('CHYRP_CODENAME', "Russet");
     define('CACHE_TWIG',     false);
     define('JAVASCRIPT',     false);
     define('ADMIN',          false);
