afdc981880e5ac672be7d5b88291b55a8e7a36b4
Daniel Pimley <dan@pimley.net>
2016-02-12 11:53:04 +0000

    Update class: removed installer and added XML validation
    Validation should protect against MITM attacks on the update channel.

--- includes/class/Update.php
+++ includes/class/Update.php
@@ -1,81 +1,44 @@
 <?php
     /**
      * Class: Update
-     * Handles updates to Chyrp Lite.
+     * Informs the user if a newer version of Chyrp Lite is available.
      */
     class Update {
         /**
          * Function: check
-         * Checks if a newer version of Chyrp Lite is available.
+         * Checks the update channel.
          */
         public static function check() {
-            $config = Config::current();
-
             $xml = simplexml_load_string(get_remote(UPDATE_XML, 3));
-            $config->set("check_updates_last", time());
+            Config::current()->set("check_updates_last", time());
 
-            if ($xml == false) {
-                Flash::warning(__("Unable to check for updates.").
-                                  ' <a href="'.UPDATE_PAGE.'" target="_blank">'.__("Go to GitHub &rarr;").'</a>');
-                return;
-            }
+            if (!self::validate($xml))
+                return Flash::warning(__("Unable to check for new Chyrp Lite versions.").
+                                         ' <a href="'.UPDATE_PAGE.'" target="_blank">'.
+                                         __("Go to GitHub &rarr;").'</a>');
 
-            foreach ($xml->channel->item as $item) {
-                if (version_compare(CHYRP_VERSION, $item->version, "<") and version_compare(PHP_VERSION, $item->requires, ">=")) {
-                    #if ($config->install_updates and class_exists("ZipArchive")) {
-                    #    if (!Flash::exists())
-                    #        self::install($item->downloadurl);
-                    #} else
-                        Flash::message(_f("Chyrp Lite v%s is available.", $item->version).
-                                          ' <a href="'.$item->updateurl.'" target="_blank">'.__("Go to GitHub &rarr;").'</a>');
-
-                    break;
-                }
-            }
+            foreach ($xml->channel->item as $item)
+                if (version_compare(CHYRP_VERSION, $item->version, "<"))
+                    return Flash::message(_f("Chyrp Lite &#8220;%s&#8221; is available.", fix($item->codename, true)).
+                                             ' <a href="'.fix($item->updateurl, true).'" target="_blank">'.
+                                             __("Go to GitHub &rarr;").'</a>');
         }
 
         /**
-         * Function: install
-         * Download and install Chyrp Lite updates from GitHub.
+         * Function: validate
+         * Validates the XML dataset.
          */
-        private static function install($url) {
-            if (DEBUG)
-                error_log("INSTALLING UPDATE: ".$url); 
+        private static function validate($xml) {
+            if ($xml === false or !isset($xml->channel->item))
+                return false;
 
-            $filename = upload_from_url($url, 3, 30);
-            $filepath = uploaded($filename, false);
+            foreach ($xml->channel->item as $item)
+                if (!isset($item->version) or
+                    !isset($item->codename) or
+                    !isset($item->updateurl) or
+                    !preg_match("~^".preg_quote(UPDATE_PAGE, "~")."~", $item->updateurl))
+                    return false;
 
-            $zip = new ZipArchive;
-            $err = $zip->open($filepath);
-
-            if ($err === true) {
-                for ($i=0; $i < $zip->numFiles; $i++) { 
-                    $name = $zip->getNameIndex($i);
-
-                    if ($name != rtrim($name, "/"))
-                        continue; # Skip this item because it is a folder.
-
-                    $folders = explode("/", $name);
-                    array_shift($folders); # Disregard the base directory.
-
-                    $itemname = array_pop($folders);
-                    $itempath = MAIN_DIR;
-
-                    foreach ($folders as $folder) {
-                        $itempath.= DIR.$folder;
-
-                        if (!file_exists($itempath))
-                            mkdir($itempath, 0755);
-                    }
-
-                    copy("zip://".$filepath."#".$name, $itempath.DIR.$itemname);
-                }
-
-                $zip->close();
-                unlink($filepath);
-                redirect("/upgrade.php?upgrade=yes", true);
-                exit;
-            } else
-                error(__("Error"), _f("Failed to install Chyrp Lite update because of ZipArchive error: <code>%s</code>", zip_errors($err)));
+            return true;
         }
     }
