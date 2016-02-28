6259489dfd231a093b0fe3769c91d33d445c7ce0
Daniel Pimley <dan@pimley.net>
2016-02-28 15:08:50 +0000

    Tags module: show 404 for no results on form submission

--- modules/tags/tags.php
+++ modules/tags/tags.php
@@ -287,7 +287,7 @@
             $post = new Post($_POST['id']);
 
             if ($post->no_results)
-                Flash::warning(__("Post not found."), "/admin/?action=manage_tags");
+                show_404();
 
             if (!$post->editable())
                 show_403(__("Access Denied"), __("You do not have sufficient privileges to edit this post."));
