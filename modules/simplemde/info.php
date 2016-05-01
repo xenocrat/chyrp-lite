8d9d715e94875b2565222c1775099b5ea522fb81
Daniel Pimley <dan@pimley.net>
2016-05-01 10:26:16 +0100

    Modules: renamed simple_markdown to simplemde

---
+++ modules/simplemde/info.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    "name"          => __("SimpleMDE", "simplemde"),
+    "url"           => "https://simplemde.com/",
+    "version"       => 1.0,
+    "description"   => __("WYSIWYG-esque editor that allows authors inexperienced with Markdown to use familiar toolbar buttons and shortcuts.", "simplemde"),
+    "author"        => array(
+        "name"      => "Daniel Pimley",
+        "url"       => "http://www.pimley.net/")
+);
