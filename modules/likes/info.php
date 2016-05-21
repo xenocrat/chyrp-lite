9f60c3d500c7a524d93b2e9e2068730386fcacf4
Daniel Pimley <dan@pimley.net>
2016-05-21 17:33:07 +0100

    Likes module: bumped version number

--- modules/likes/info.php
+++ modules/likes/info.php
@@ -2,7 +2,7 @@
 return array(
     "name"          => __("Likes", "likes"),
     "url"           => "http://chyrplite.net/",
-    "version"       => 1.4,
+    "version"       => 1.5,
     "description"   => __("Allow users to &#8220;like&#8221; a post.", "likes"),
     "author"        => array(
         "name"      => "Chyrp team",
