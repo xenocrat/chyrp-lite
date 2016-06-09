88a72e4dce0cd945bcd94bb3dbf97a96905f43e7
Daniel Pimley <dan@pimley.net>
2016-06-09 19:57:28 +0100

    Download.php: bring in common.php but disable OB
    Using the new USE_OB constant to simplify this script: we require
    common.php for the full feature set but disable output buffering for
    maximum performance when delivering very large files.

--- includes/download.php
+++ includes/download.php
@@ -4,35 +4,9 @@
      * Send an uploaded file to the visitor.
      */
 
-    define('DEBUG',        false);
-    define('JAVASCRIPT',   false);
-    define('MAIN',         false);
-    define('ADMIN',        false);
-    define('AJAX',         false);
-    define('XML_RPC',      false);
-    define('UPGRADING',    false);
-    define('INSTALLING',   false);
-    define('TESTER',       isset($_SERVER['HTTP_USER_AGENT']) and $_SERVER['HTTP_USER_AGENT'] == "TESTER");
-    define('DIR',          DIRECTORY_SEPARATOR);
-    define('MAIN_DIR',     dirname(dirname(__FILE__)));
-    define('INCLUDES_DIR', dirname(__FILE__));
+    define('USE_OB', false);
 
-    # Constant: JSON_PRETTY_PRINT
-    # Define a safe value to avoid warnings pre-5.4
-    if (!defined('JSON_PRETTY_PRINT'))
-        define('JSON_PRETTY_PRINT', 0);
-
-    # Constant: JSON_UNESCAPED_SLASHES
-    # Define a safe value to avoid warnings pre-5.4
-    if (!defined('JSON_UNESCAPED_SLASHES'))
-        define('JSON_UNESCAPED_SLASHES', 0);
-
-    require_once "error.php";
-    require_once "helpers.php";
-    require_once "class".DIR."Config.php";
-
-    # Sanitize input depending on magic_quotes_gpc's enabled status.
-    sanitize_input($_GET);
+    require_once "common.php";
 
     if (isset($_SERVER["REQUEST_METHOD"]) and $_SERVER["REQUEST_METHOD"] !== "GET")
         error(__("Error"), __("This resource accepts GET requests only."), null, 405);
