9036690ac63a73013a62a2b9962fb8d99936dc06
Daniel Pimley <dan@pimley.net>
2016-06-26 21:48:48 +0100

    Installer: making the time info localizable

--- install.php
+++ install.php
@@ -797,7 +797,7 @@ foreach ($errors as $error)
                     <select name="timezone" id="timezone">
                     <?php foreach (timezones() as $zone): ?>
                         <option value="<?php echo $zone["name"]; ?>"<?php selected($zone["name"], $timezone); ?>>
-                            <?php echo when("%I:%M %p on %B %d, %Y", $zone["now"], true); ?> &mdash;
+                            <?php echo when(__("%I:%M %p on %B %d, %Y"), $zone["now"], true); ?> &mdash;
                             <?php echo str_replace(array("_", "St "), array(" ", "St. "), $zone["name"]); ?>
                         </option>
                     <?php endforeach; ?>
