d9fc865ddbcdaa9bb6fabd35ecfbaa024b0d602e
Daniel Pimley <dan@pimley.net>
2016-07-01 00:47:01 +0100

    User: password hashing and checking improvements
    Not allowing the clear text check, don’t like the security
    implications; always use random() to generate the salt and let it do
    its best to be secure; using hash_equals() if available to check
    passwords, to avoid timing attacks.

--- includes/model/User.php
+++ includes/model/User.php
@@ -50,16 +50,9 @@
 
             if ($check->no_results)
                 return false;
-            else {
-                if (self::checkPassword($password, $check->password))
-                    return true;
-                elseif ($password == $check->password) {
-                    # Password is stored in clear text, perhaps from an import.
-                    # Accept it and update the stored value with a secure hash.
-                    $check->update(null, self::hashPassword($password));
-                    return true;
-                }
-            }
+
+            if (self::checkPassword($password, $check->password))
+                return true;
 
             return false;
         }
@@ -189,28 +182,26 @@
 
         /**
          * Function: hashPassword
-         * Creates a secure hash of a user's password.
+         * Creates a hash of a user's password for the database.
          *
          * Parameters:
          *     $password - The unhashed password.
          * 
          * Returns:
-         *     The securely hashed password to be stored in the database.
+         *     The hashed password.
          *
          * Notes:
-         *     Base64 encodes 6 bits per character so we generate 16*6/8=12 bytes for the salt.
+         *     <random> tries to be cryptographically secure.
          */
         static function hashPassword($password) {
-            $salt = (function_exists("openssl_random_pseudo_bytes")) ?
-                base64_encode(openssl_random_pseudo_bytes(12)) : random(16);
-
-            $param = '$6$'.$salt; # Selecting SHA-512 (>=PHP 5.3.2)
-            return crypt($password,$param);
+            $salt = random(16);
+            $prefix = '$6$'; # SHA-512
+            return crypt($password, $prefix.$salt);
         }
 
         /**
          * Function: checkPassword
-         * Checks a given password against the stored hash.
+         * Checks a given password against the user's stored hash.
          *
          * Parameters:
          *     $password - The unhashed password given during a login attempt.
@@ -220,7 +211,10 @@
          *     @true@ or @false@
          */
         static function checkPassword($password, $storedHash) {
-            return (crypt($password, $storedHash) == $storedHash);
+            if (function_exists("hash_equals"))
+                return hash_equals(crypt($password, $storedHash), $storedHash);
+            else
+                return (crypt($password, $storedHash) == $storedHash);
         }
 
     }
