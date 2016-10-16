ca2fd6feeba1219916c2d8ffcdec6369a6ecf0d6
Daniel Pimley <dan@pimley.net>
2016-10-16 12:22:48 +0100

    Likes module: neater Flash redirect and fixed route for likes
    We should target config->url because we want MainController to catch
    this request.

--- modules/likes/likes.php
+++ modules/likes/likes.php
@@ -50,7 +50,7 @@
                                "likeWithText" => isset($_POST['likeWithText']),
                                "likeImage" => $_POST['likeImage']));
 
-            Flash::notice(__("Settings updated."), "/?action=like_settings");
+            Flash::notice(__("Settings updated."), "like_settings");
         }
 
         public function settings_nav($navs) {
@@ -185,7 +185,7 @@
             if (!$like->resolve()) {
                 if ($visitor->group->can("like_post")) {
                     $html.= "<a class=\"likes like\" href=\"".
-                                $config->chyrp_url."/?action=like&post_id=".
+                                $config->url."/?action=like&post_id=".
                                 $post->id."\" data-post_id=\"".
                                 $post->id."\">".
                                 "<img src=\"".$module_like["likeImage"]."\" alt='Likes icon'>";
@@ -210,7 +210,7 @@
             } else {
                 if ($visitor->group->can("unlike_post")) {
                     $html.= "<a class=\"likes liked\" href=\"".
-                                $config->chyrp_url."/?action=unlike&post_id=".
+                                $config->url."/?action=unlike&post_id=".
                                 $post->id."\" data-post_id=\"".
                                 $post->id."\">".
                                 "<img src=\"".$module_like["likeImage"]."\" alt='Likes icon'>";
