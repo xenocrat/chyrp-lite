4f498ee6a8d99ca4e33d597adbbe12512683bb66
Daniel Pimley <dan@pimley.net>
2016-10-30 09:53:58 +0000

    Migrator module: changed module description

--- modules/migrator/info.php
+++ modules/migrator/info.php
@@ -3,7 +3,7 @@ return array(
     "name"          => __("Migration Assistant", "migrator"),
     "url"           => "http://chyrplite.net/",
     "version"       => 1.0,
-    "description"   => __("Allows import from Wordpress, MovableType, TextPattern, and Tumblr.", "migrator"),
+    "description"   => __("Enables import from Wordpress, MovableType, TextPattern, and Tumblr.", "migrator"),
     "author"        => array(
         "name"      => "Daniel Pimley",
         "url"       => "http://www.pimley.net/")
