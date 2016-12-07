e36970d4a7ce7b9daadad25395947da6f883f9a6
Daniel Pimley <dan@pimley.net>
2016-12-07 23:09:48 +0000

    Error: report in plain text if inside a nested output buffer

--- includes/error.php
+++ includes/error.php
@@ -144,7 +144,7 @@
         }
 
         # Report in plain text if desirable or necessary because of a deep error.
-        if (TESTER or XML_RPC or AJAX or JAVASCRIPT or
+        if (TESTER or XML_RPC or AJAX or JAVASCRIPT or ob_get_level() > 1 or
             !function_exists("__") or
             !function_exists("_f") or
             !function_exists("fallback") or
