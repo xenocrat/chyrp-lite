99407c2261d612dab5b7f3c3bec1dcf329205f55
Daniel Pimley <dan@pimley.net>
2016-12-13 20:42:48 +0000

    Post Views module: view count can be downloaded as CSV file

--- modules/post_views/post_views.php
+++ modules/post_views/post_views.php
@@ -17,11 +17,15 @@
         }
 
         public function manage_posts_column_header() {
-            echo '<th class="post_views value">'.__("Views", "post_views").'</th>';
+            echo '<th class="post_views value">'.__("View Count", "post_views").'</th>';
         }
 
         public function manage_posts_column($post) {
-            echo '<td class="post_views value">'.$post->view_count.'</td>';
+            if ($post->view_count > 0)
+                echo '<td class="post_views value">'.
+                        '<a href="'.url("download_views/id/".$post->id).'">'.$post->view_count.'</a></td>';
+            else
+                echo '<td class="post_views value">'.$post->view_count.'</td>';
         }
 
         public function post($post) {
@@ -30,6 +34,26 @@
 
         static function delete_post($post) {
             SQL::current()->delete("views", array("post_id" => $post->id));
+        }
+
+        public function admin_download_views() {
+            if (empty($_GET['id']) or !is_numeric($_GET['id']))
+                error(__("No ID Specified"), __("An ID is required to download a view count.", "post_views"), null, 400);
+
+            $post = new Post($_GET['id'], array("drafts" => true));
+
+            if ($post->no_results)
+                show_404(__("Not Found"), __("Post not found."));
+
+            $data = View::find(array("where" => array("post_id" => $post->id)));
+
+            $filename = sanitize(camelize($post->title()), false, true)."_View_Count_".date("Y-m-d");
+            $filedata = "id,post_id,user_id,created_at\r\n";
+
+            foreach ($data as $datum)
+                $filedata.= $datum->id.",".$datum->post_id.",".$datum->user_id.",".$datum->created_at."\r\n";
+
+            file_attachment($filedata, $filename.".csv");
         }
 
         public function post_view_count_attr($attr, $post) {
