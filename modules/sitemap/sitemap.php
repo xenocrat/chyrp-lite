9388a0b846ca07b784ba65d248b9a607ba8ab1e7
Daniel Pimley <dan@pimley.net>
2016-12-14 21:05:01 +0000

    Sitemap module: fixed Flash redirect snafu

--- modules/sitemap/sitemap.php
+++ modules/sitemap/sitemap.php
@@ -59,7 +59,7 @@
                                "pages_changefreq" => $_POST['pages_changefreq'],
                                "posts_changefreq" => $_POST['posts_changefreq']));
 
-            Flash::notice(__("Settings updated."), "sitemap");
+            Flash::notice(__("Settings updated."), "sitemap_settings");
         }
 
         /**
