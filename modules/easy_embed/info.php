510604a4e14a9022eba50bb6aa080f134ee0af12
Daniel Pimley <dan@pimley.net>
2016-12-24 00:58:41 +0000

    Easy Embed module: yet more metadata changes

--- modules/easy_embed/info.php
+++ modules/easy_embed/info.php
@@ -3,7 +3,7 @@ return array(
     "name"          => __("Easy Embed", "easy_embed"),
     "url"           => "http://chyrplite.net/",
     "version"       => 1.0,
-    "description"   => __("Embed a video in your blog content by pasting its URL surrounded by <code>&lt;!-- --&gt;</code>.", "easy_embed"),
+    "description"   => __("Embed a video in your blog content by pasting its URL surrounded by <code>&lt;!--</code> and <code>--&gt;</code>.", "easy_embed"),
     "author"        => array(
         "name"      => "Daniel Pimley",
         "url"       => "http://www.pimley.net/")
