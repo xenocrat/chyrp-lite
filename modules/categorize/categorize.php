6f65f57a2241b95a89b1ec933ca42142e26fd071
Daniel Pimley <dan@pimley.net>
2016-12-29 22:36:13 +0000

    Categorize module: code cleaning

--- modules/categorize/categorize.php
+++ modules/categorize/categorize.php
@@ -33,7 +33,7 @@
 
         public function metaWeblog_getPost($struct, $post) {
             if (!empty($post->category))
-                    $struct['categories'] = array($post->category->name);
+                $struct['categories'] = array($post->category->name);
 
             return $struct;
         }
