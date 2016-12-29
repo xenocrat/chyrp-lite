49b744e3b10036301441f5a5f67f5b5d3b6e5adc
Daniel Pimley <dan@pimley.net>
2016-12-29 09:08:57 +0000

    Comments module: improved XMLRPC support

--- modules/comments/comments.php
+++ modules/comments/comments.php
@@ -696,13 +696,15 @@
         }
 
         public function metaWeblog_getPost($struct, $post) {
-            $struct['mt_allow_comments'] = isset($post->comment_status) ? intval($post->comment_status == "open") : 1 ;
+            $struct['mt_allow_comments'] = isset($post->comment_status and $post->comment_status == "open") ? "open" : "closed" ;
             return $struct;
         }
 
         public function metaWeblog_editPost_preQuery($struct, $post = null) {
             if (isset($struct['mt_allow_comments']))
-                $_POST['option']['comment_status'] = ($struct['mt_allow_comments'] == 1) ? "open" : "closed" ;
+                $_POST['option']['comment_status'] = ($struct['mt_allow_comments'] == "open") ? "open" : "closed" ;
+            else
+                $_POST['option']['comment_status'] = "closed";
         }
 
         public function post($post) {
