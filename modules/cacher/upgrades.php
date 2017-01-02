2bb46838953f944c331fc84f6edfbc947cc46fbc
Daniel Pimley <dan@pimley.net>
2017-01-02 01:57:23 +0000

    Cacher module: code cleaning

--- modules/cacher/upgrades.php
+++ modules/cacher/upgrades.php
@@ -1,13 +1,13 @@
 <?php
+    if (!defined('UPGRADING') or !UPGRADING)
+        exit;
+
     /**
      * Function: remove_memcached_hosts
      * Removes the cache_memcached_hosts config setting.
      *
      * Versions: 2015.06 => 2015.07
      */
-    if (!defined('UPGRADING') or !UPGRADING)
-        exit;
-
     function remove_memcached_hosts() {
         Config::current()->remove("cache_memcached_hosts");
     }
