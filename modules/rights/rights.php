b33fc5dda76d8fa7de11ed4f80e58546938405cf
Daniel Pimley <dan@pimley.net>
2017-01-22 11:17:55 +0000

    Rights module: changes to __uninstall() and code cleaning
    We can delete our post attributes much more efficiently with these
    three statements.

--- modules/rights/rights.php
+++ modules/rights/rights.php
@@ -4,30 +4,47 @@
             if ($confirm) {
                 $sql = SQL::current();
 
-                foreach($sql->select("post_attributes",
-                                     "*",
-                                     array("name" => "rights_title"))->fetchAll() as $post)  {
-                    $sql->delete("post_attributes", array("name" => "rights_title",
-                                                          "post_id" => $post["post_id"]));
-                }
-
-                foreach($sql->select("post_attributes",
-                                     "*",
-                                     array("name" => "rights_holder"))->fetchAll() as $post)  {
-                    $sql->delete("post_attributes", array("name" => "rights_holder",
-                                                          "post_id" => $post["post_id"]));
-                }
-
-                foreach($sql->select("post_attributes",
-                                     "*",
-                                     array("name" => "rights_licence"))->fetchAll() as $post)  {
-                    $sql->delete("post_attributes", array("name" => "rights_licence",
-                                                          "post_id" => $post["post_id"]));
-                }
+                $sql->delete("post_attributes", array("name" => "rights_title"));
+                $sql->delete("post_attributes", array("name" => "rights_holder"));
+                $sql->delete("post_attributes", array("name" => "rights_licence"));
             }
         }
 
         public function post_options($fields, $post = null) {
+            $licences = array(array("name" => __("All rights reserved", "rights"),
+                                    "value" => "All rights reserved",
+                                    "selected" => ($post ? $post->rights_licence == "All rights reserved" : true)),
+                              array("name" => __("Creative Commons BY", "rights"),
+                                    "value" => "Creative Commons BY",
+                                    "selected" => ($post ? $post->rights_licence == "Creative Commons BY" : false)),
+                              array("name" => __("Creative Commons BY-ND", "rights"),
+                                    "value" => "Creative Commons BY-ND",
+                                    "selected" => ($post ? $post->rights_licence == "Creative Commons BY-ND" : false)),
+                              array("name" => __("Creative Commons BY-SA", "rights"),
+                                    "value" => "Creative Commons BY-SA",
+                                    "selected" => ($post ? $post->rights_licence == "Creative Commons BY-SA" : false)),
+                              array("name" => __("Creative Commons BY-NC", "rights"),
+                                    "value" => "Creative Commons BY-NC",
+                                    "selected" => ($post ? $post->rights_licence == "Creative Commons BY-NC" : false)),
+                              array("name" => __("Creative Commons BY-NC-ND", "rights"),
+                                    "value" => "Creative Commons BY-NC-ND",
+                                    "selected" => ($post ? $post->rights_licence == "Creative Commons BY-NC-ND" : false)),
+                              array("name" => __("Creative Commons BY-NC-SA", "rights"),
+                                    "value" => "Creative Commons BY-NC-SA",
+                                    "selected" => ($post ? $post->rights_licence == "Creative Commons BY-NC-SA" : false)),
+                              array("name" => __("Creative Commons CC0", "rights"),
+                                    "value" => "Creative Commons CC0",
+                                    "selected" => ($post ? $post->rights_licence == "Creative Commons CC0" : false)),
+                              array("name" => __("Orphan Work", "rights"),
+                                    "value" => "Orphan Work",
+                                    "selected" => ($post ? $post->rights_licence == "Orphan Work" : false)),
+                              array("name" => __("Public Domain", "rights"),
+                                    "value" => "Public Domain",
+                                    "selected" => ($post ? $post->rights_licence == "Public Domain" : false)),
+                              array("name" => __("Crown Copyright", "rights"),
+                                    "value" => "Crown Copyright",
+                                    "selected" => ($post ? $post->rights_licence == "Crown Copyright" : false)));
+
             $fields[] = array("attr" => "option[rights_title]",
                               "label" => __("Original Work", "rights"),
                               "type" => "text",
@@ -42,39 +59,7 @@
                               "label" => __("License", "rights"),
                               "help" => "choosing_a_licence",
                               "type" => "select",
-                              "options" => array(array("name" => __("All rights reserved", "rights"),
-                                                       "value" => "All rights reserved",
-                                                       "selected" => ($post ? $post->rights_licence == "All rights reserved" : true)),
-                                                 array("name" => __("Creative Commons BY", "rights"),
-                                                       "value" => "Creative Commons BY",
-                                                       "selected" => ($post ? $post->rights_licence == "Creative Commons BY" : false)),
-                                                 array("name" => __("Creative Commons BY-ND", "rights"),
-                                                       "value" => "Creative Commons BY-ND",
-                                                       "selected" => ($post ? $post->rights_licence == "Creative Commons BY-ND" : false)),
-                                                 array("name" => __("Creative Commons BY-SA", "rights"),
-                                                       "value" => "Creative Commons BY-SA",
-                                                       "selected" => ($post ? $post->rights_licence == "Creative Commons BY-SA" : false)),
-                                                 array("name" => __("Creative Commons BY-NC", "rights"),
-                                                       "value" => "Creative Commons BY-NC",
-                                                       "selected" => ($post ? $post->rights_licence == "Creative Commons BY-NC" : false)),
-                                                 array("name" => __("Creative Commons BY-NC-ND", "rights"),
-                                                       "value" => "Creative Commons BY-NC-ND",
-                                                       "selected" => ($post ? $post->rights_licence == "Creative Commons BY-NC-ND" : false)),
-                                                 array("name" => __("Creative Commons BY-NC-SA", "rights"),
-                                                       "value" => "Creative Commons BY-NC-SA",
-                                                       "selected" => ($post ? $post->rights_licence == "Creative Commons BY-NC-SA" : false)),
-                                                 array("name" => __("Creative Commons CC0", "rights"),
-                                                       "value" => "Creative Commons CC0",
-                                                       "selected" => ($post ? $post->rights_licence == "Creative Commons CC0" : false)),
-                                                 array("name" => __("Orphan Work", "rights"),
-                                                       "value" => "Orphan Work",
-                                                       "selected" => ($post ? $post->rights_licence == "Orphan Work" : false)),
-                                                 array("name" => __("Public Domain", "rights"),
-                                                       "value" => "Public Domain",
-                                                       "selected" => ($post ? $post->rights_licence == "Public Domain" : false)),
-                                                 array("name" => __("Crown Copyright", "rights"),
-                                                       "value" => "Crown Copyright",
-                                                       "selected" => ($post ? $post->rights_licence == "Crown Copyright" : false))));
+                              "options" => $licences);
 
             return $fields;
         }
