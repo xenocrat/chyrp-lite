1869cf65634f19ca240fa04a77e7eb77fb98d5f8
Daniel Pimley <dan@pimley.net>
2017-04-07 13:50:18 +0100

    Cascade module: moved config settings into dedicated array
    Bumped version number.

---
+++ modules/cascade/upgrades.php
@@ -0,0 +1,22 @@
+<?php
+    if (!defined('UPGRADING') or !UPGRADING)
+        exit;
+
+    /**
+     * Function: cascade_migrate_config
+     * Moves config settings into an array.
+     *
+     * Versions: 2017.01 => 2017.02
+     */
+    function cascade_migrate_config() {
+        $config = Config::current();
+
+        if (isset($config->ajax_scroll_auto)) {
+            $set = $config->set("module_cascade", array("ajax_scroll_auto" => $config->ajax_scroll_auto));
+
+            if ($set !== false)
+                $config->remove("ajax_scroll_auto");
+        }
+    }
+
+    cascade_migrate_config();
