e40c442c3d904bc41eac24d52a61c00746c69136
Daniel Pimley <dan@pimley.net>
2017-06-07 20:21:50 +0100

    SimpleMDE module: fixed admin_head() trigger response
    Echo instead of returning – if there are multiple responders, only the
    last return value will be outputted.

--- modules/simplemde/simplemde.php
+++ modules/simplemde/simplemde.php
@@ -6,15 +6,15 @@
             if (!$config->enable_markdown)
                 return;
 
-            return "<!-- SimpleMDE -->\n".
-                   '<link rel="stylesheet" href="'.$config->chyrp_url.'/modules/simplemde/simplemde.min.css" type="text/css" media="all">'."\n".
-                   '<script src="'.$config->chyrp_url.'/modules/simplemde/simplemde.min.js" type="text/javascript" charset="UTF-8"></script>'."\n".
-                   '<script type="text/javascript">'."\n".
-                   '    $(function() {'."\n".
-                   '        $("textarea[data-markdown]").each(function() {'."\n".
-                   '            new SimpleMDE({ element: $(this)[0], forceSync: true });'."\n".
-                   '        });'."\n".
-                   '    });'."\n".
-                   '</script>';
+            echo "<!-- SimpleMDE -->\n".
+                 '<link rel="stylesheet" href="'.$config->chyrp_url.'/modules/simplemde/simplemde.min.css" type="text/css" media="all">'."\n".
+                 '<script src="'.$config->chyrp_url.'/modules/simplemde/simplemde.min.js" type="text/javascript" charset="UTF-8"></script>'."\n".
+                 '<script type="text/javascript">'."\n".
+                 '    $(function() {'."\n".
+                 '        $("textarea[data-markdown]").each(function() {'."\n".
+                 '            new SimpleMDE({ element: $(this)[0], forceSync: true });'."\n".
+                 '        });'."\n".
+                 '    });'."\n".
+                 '</script>';
         }
     }
