7e275562c44691bccac482e33173366cdac304a2
Daniel Pimley <dan@pimley.net>
2017-09-16 17:06:46 +0100

    MAPTCHA module: added more arithmetic variations

--- modules/maptcha/maptcha.php
+++ modules/maptcha/maptcha.php
@@ -12,14 +12,52 @@
             $maptcha_hashkey = Config::current()->module_maptcha["maptcha_hashkey"];
 
             $x = rand(1,9);
-            $y = rand(1,9);
+            $y = rand(1,$x);
+            $z = rand(1,9);
 
-            return '<label for="maptcha_response">'.
-                   _f("How much is %d + %d ?", array($x, $y), "maptcha").'</label>'."\n".
+            switch ($z) {
+                case 1:
+                    $label = _f("How much is %d&nbsp;&#43;&nbsp;%d&nbsp;?", array($x, $y), "maptcha");
+                    $value = sha1(strval($x + $y).$maptcha_hashkey);
+                    break;
+                case 2:
+                    $label = _f("How much is %d&nbsp;&#8722;&nbsp;%d&nbsp;?", array($x, $y), "maptcha");
+                    $value = sha1(strval($x - $y).$maptcha_hashkey);
+                    break;
+                case 3:
+                    $label = _f("How much is %d&nbsp;&#215;&nbsp;%d&nbsp;?", array($x, $y), "maptcha");
+                    $value = sha1(strval($x * $y).$maptcha_hashkey);
+                    break;
+                case 4:
+                    $label = _f("How much is %d&nbsp;&plus;&nbsp;%d&nbsp;?", array($x, $y), "maptcha");
+                    $value = sha1(strval($x + $y).$maptcha_hashkey);
+                    break;
+                case 5:
+                    $label = _f("How much is %d&nbsp;&minus;&nbsp;%d&nbsp;?", array($x, $y), "maptcha");
+                    $value = sha1(strval($x - $y).$maptcha_hashkey);
+                    break;
+                case 6:
+                    $label = _f("How much is %d&nbsp;&times;&nbsp;%d&nbsp;?", array($x, $y), "maptcha");
+                    $value = sha1(strval($x * $y).$maptcha_hashkey);
+                    break;
+                case 7:
+                    $label = _f("How much is %d&nbsp;&#x0002B;&nbsp;%d&nbsp;?", array($x, $y), "maptcha");
+                    $value = sha1(strval($x + $y).$maptcha_hashkey);
+                    break;
+                case 8:
+                    $label = _f("How much is %d&nbsp;&#x02212;&nbsp;%d&nbsp;?", array($x, $y), "maptcha");
+                    $value = sha1(strval($x - $y).$maptcha_hashkey);
+                    break;
+                case 9:
+                    $label = _f("How much is %d&nbsp;&#x000D7;&nbsp;%d&nbsp;?", array($x, $y), "maptcha");
+                    $value = sha1(strval($x * $y).$maptcha_hashkey);
+                    break;
+            }
+
+            return '<label for="maptcha_response">'.$label.'</label>'."\n".
                    '<input type="number" name="maptcha_response" value="" placeholder="'.
                    __("Yay mathemetics!", "maptcha").'">'."\n".
-                   '<input type="hidden" name="maptcha_challenge" value="'.
-                   sha1(strval($x + $y).$maptcha_hashkey).'">'."\n";
+                   '<input type="hidden" name="maptcha_challenge" value="'.$value.'">'."\n";
         }
 
         static function checkCaptcha() {
