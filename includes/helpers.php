064f1148bcf0c3facee205353e607e949fc2f514
Daniel Pimley <dan@pimley.net>
2017-12-24 15:00:50 +0000

    Helpers: changes to get_remote(), pingback_url()
    Ensure we always close streams for improved memory footprint; removed
    cURL and file_get_contents() to simplify implementation.

--- includes/helpers.php
+++ includes/helpers.php
@@ -1221,66 +1221,54 @@
      */
     function get_remote($url, $redirects = 0, $timeout = 10) {
         extract(parse_url($url), EXTR_SKIP);
-        $content = "";
+        fallback($path, "/");
+        fallback($scheme, "http");
+        fallback($port, ($scheme == "https") ? 443 : 80);
 
-        if (ini_get("allow_url_fopen")) {
-            $context = stream_context_create(array("http" => array("follow_location" => ($redirects == 0) ? 0 : 1 ,
-                                                                   "max_redirects" => $redirects,
-                                                                   "timeout" => $timeout,
-                                                                   "protocol_version" => 1.1,
-                                                                   "user_agent" => CHYRP_IDENTITY)));
-            $content = @file_get_contents($url, false, $context);
-        } elseif (function_exists("curl_init")) {
-            $handle = curl_init();
-            curl_setopt($handle, CURLOPT_URL, $url);
-            curl_setopt($handle, CURLOPT_HEADER, false);
-            curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($handle, CURLOPT_TIMEOUT, $timeout + 60);
-            curl_setopt($handle, CURLOPT_FOLLOWLOCATION, ($redirects == 0) ? false : true );
-            curl_setopt($handle, CURLOPT_MAXREDIRS, $redirects);
-            curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, $timeout);
-            curl_setopt($handle, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-            curl_setopt($handle, CURLOPT_USERAGENT, CHYRP_IDENTITY);
-            $content = curl_exec($handle);
-            curl_close($handle);
-        } else {
-            fallback($path, '/');
-            fallback($port, 80);
+        if (isset($query))
+            $path.= '?'.$query;
 
-            if (isset($query))
-                $path.= '?'.$query;
+        if (!isset($host))
+            return false;
 
-            $connect = @fsockopen($host, $port, $errno, $errstr, $timeout);
+        $prefix  = ($scheme == "https") ? "tls://" : "tcp://" ;
+        $connect = @fsockopen($prefix.$host, $port, $errno, $errstr, $timeout);
 
-            if ($connect) {
-                $remote_headers = "";
+        if (!$connect)
+            return false;
 
-                # Send the GET headers.
-                fwrite($connect, "GET ".$path." HTTP/1.1\r\n");
-                fwrite($connect, "Host: ".$host."\r\n");
-                fwrite($connect, "User-Agent: ".CHYRP_IDENTITY."\r\n\r\n");
+        $remote_headers = "";
+        $remote_content = "";
 
-                while (!feof($connect) and strpos($remote_headers, "\r\n\r\n") === false)
-                    $remote_headers.= fgets($connect);
+        # Send the GET headers.
+        fwrite($connect,
+            "GET ".$path." HTTP/1.1\r\n".
+            "Host: ".$host."\r\n".
+            "Connection: close"."\r\n".
+            "User-Agent: ".CHYRP_IDENTITY."\r\n\r\n");
 
-                while (!feof($connect))
-                    $content.= fgets($connect);
+        # Receive response headers.
+        while (!feof($connect) and strpos($remote_headers, "\r\n\r\n") === false)
+            $remote_headers.= fgets($connect);
 
+        # Search for 301 or 302 header and recurse with new location unless redirects are exhausted.
+        if ($redirects > 0 and preg_match("~^HTTP/[0-9]\.[0-9] 30[1-2]~m", $remote_headers)
+                           and preg_match("~^Location:(.+)$~mi", $remote_headers, $matches)) {
+
+            $location = trim($matches[1]);
+
+            if (is_url($location)) {
                 fclose($connect);
-
-                # Search for 301 or 302 header and recurse with new location unless redirects are exhausted.
-                if ($redirects > 0 and preg_match("~^HTTP/[0-9]\.[0-9] 30[1-2]~m", $remote_headers)
-                                   and preg_match("~^Location:(.+)$~mi", $remote_headers, $matches)) {
-
-                    $location = trim($matches[1]);
-
-                    if (is_url($location))
-                        $content = get_remote($location, $redirects - 1, $timeout);
-                }
+                return get_remote($location, $redirects - 1, $timeout);
             }
         }
 
-        return $content;
+        # Receive the response content.
+        while (!feof($connect))
+            $remote_content.= fgets($connect);
+
+        fclose($connect);
+        return $remote_content;
     }
 
     /**
@@ -1344,10 +1332,9 @@
      */
     function pingback_url($url) {
         extract(parse_url($url), EXTR_SKIP);
-
-        $config = Config::current();
-        fallback($path, '/');
-        fallback($port, 80);
+        fallback($path, "/");
+        fallback($scheme, "http");
+        fallback($port, ($scheme == "https") ? 443 : 80);
 
         if (isset($query))
             $path.= '?'.$query;
@@ -1355,47 +1342,45 @@
         if (!isset($host))
             return false;
 
-        $connect = @fsockopen($host, $port, $errno, $errstr, 2);
+        $prefix  = ($scheme == "https") ? "tls://" : "tcp://" ;
+        $connect = @fsockopen($prefix.$host, $port, $errno, $errstr, 3);
 
         if (!$connect)
             return false;
 
         $remote_headers = "";
-        $remote_bytes = 0;
+        $remote_content = "";
 
         # Send the GET headers.
-        fwrite($connect, "GET ".$path." HTTP/1.1\r\n");
-        fwrite($connect, "Host: $host\r\n");
-        fwrite($connect, "User-Agent: ".CHYRP_IDENTITY."\r\n\r\n");
+        fwrite($connect,
+            "GET ".$path." HTTP/1.1\r\n".
+            "Host: ".$host."\r\n".
+            "Connection: close"."\r\n".
+            "User-Agent: ".CHYRP_IDENTITY."\r\n\r\n");
 
         # Check for X-Pingback header.
-        while (!feof($connect)) {
+        while (!feof($connect) and strpos($remote_headers, "\r\n\r\n") === false) {
             $line = fgets($connect);
+            $remote_headers.= $line;
 
-            if (trim($line) == "")
-                break;
-
-            $remote_headers.= trim($line)."\n";
-
-            if (preg_match("/X-Pingback:(.+)/i", $line, $matches))
-                return trim($matches[1]);
+            if (preg_match("/^X-Pingback:(.+)/i", $line, $header)) {
+                fclose($connect);
+                return trim($header[1]);
+            }
         }
 
-        # X-Pingback header not found, <link> search if the content can be parsed.
-        if (!preg_match("~Content-Type:\s+(text/html|text/sgml|text/xml|text/plain)~im", $remote_headers))
-            return false;
+        # Check <link> elements if the content can be parsed.
+        if (preg_match("~^Content-Type:\s+(text/html|text/sgml|text/xml|text/plain)~im", $remote_headers)) {
+            while (!feof($connect) and strlen($remote_content) < 2048) {
+                $line = fgets($connect);
+                $remote_content.= $line;
 
-        while (!feof($connect)) {
-            $line = fgets($connect);
-
-            if (preg_match("/<link[^>]* href=(\"[^\"]+\"|\'[^\']+\')[^>]*>/i", $line, $link))
-                if (preg_match("/ rel=(\"pingback\"|\'pingback\')/i", $link[0]))
-                    return trim($link[1], "\"'");
-
-            $remote_bytes += strlen($line);
-
-            if ($remote_bytes > 2048)
-                return false;
+                if (preg_match("/<link[^>]* href=(\"[^\"]+\"|\'[^\']+\')[^>]*>/i", $line, $link))
+                    if (preg_match("/ rel=(\"pingback\"|\'pingback\')/i", $link[0])) {
+                        fclose($connect);
+                        return trim($link[1], "\"'");
+                    }
+            }
         }
 
         fclose($connect);
