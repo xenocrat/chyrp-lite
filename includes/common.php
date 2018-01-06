885f559453c287cf9149f0a9aa5142e1f910100c
Daniel Pimley <dan@pimley.net>
2018-01-06 22:59:37 +0000

    Common: setting Strict-Transport-Security max age to 2592000
    Firefox will accept this value; Safari modifies it to 31536000.

--- includes/common.php
+++ includes/common.php
@@ -378,4 +378,4 @@
 
     # Upgrade future requests if the canonical URL is HTTPS.
     if (substr_count($config->url, "https://"))
-        header("Strict-Transport-Security: max-age=31536001");
+        header("Strict-Transport-Security: max-age=2592000");
