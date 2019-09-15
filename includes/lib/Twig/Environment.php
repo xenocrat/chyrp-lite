4189aa8d3b049e0a8301ea2ce22771b431122d8a
Daniel Pimley <dan@pimley.net>
2019-09-15 15:13:31 +0100

    Updated Twig to v1.42.3

--- includes/lib/Twig/Environment.php
+++ includes/lib/Twig/Environment.php
@@ -41,11 +41,11 @@ use Twig\TokenParser\TokenParserInterface;
  */
 class Environment
 {
-    const VERSION = '1.42.2';
-    const VERSION_ID = 14202;
+    const VERSION = '1.42.3';
+    const VERSION_ID = 14203;
     const MAJOR_VERSION = 1;
     const MINOR_VERSION = 42;
-    const RELEASE_VERSION = 2;
+    const RELEASE_VERSION = 3;
     const EXTRA_VERSION = '';
 
     protected $charset;
