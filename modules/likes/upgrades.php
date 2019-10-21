a768de20eb47fc219e43f32a93d24da9dd555d09
Daniel Pimley <dan@pimley.net>
2019-10-21 08:32:29 +0100

    Likes module: bumped version and added upgrades task
    Extract the image filename from the filepath we stored previously.

--- modules/likes/upgrades.php
+++ modules/likes/upgrades.php
@@ -15,7 +15,7 @@
             $set = $config->set("module_likes",
                                 array("show_on_index" => $config->module_like["showOnFront"],
                                       "like_with_text" => $config->module_like["likeWithText"],
-                                      "like_image" => "pink.svg"));
+                                      "like_image" => $config->module_like["likeImage"]));
 
             if ($set !== false)
                 $set = $config->remove("module_like");
@@ -25,4 +25,23 @@
         }
     }
 
+    /**
+     * Function: likes_update_config
+     * Updates config settings for for v1.2 and upwards.
+     *
+     * Versions: 2019.03 => 2019.04
+     */
+    function likes_update_config() {
+        $config = Config::current();
+        $array = $config->module_likes;
+
+        $array["like_image"] = preg_replace("~^https?://.+/modules/likes/images/(.+)$~", "$1", $array["like_image"]);
+
+        $set = $config->set("module_likes", $array);
+
+        if ($set === false)
+            error(__("Error"), __("Could not write the configuration file."));
+    }
+
     likes_migrate_config();
+    likes_update_config();
