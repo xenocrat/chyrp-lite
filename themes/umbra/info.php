e126fe87318a9766e0ca221f8160569027537f9d
Daniel Pimley <dan@pimley.net>
2019-11-06 21:41:56 +0000

    Umbra: bumped version number

--- themes/umbra/info.php
+++ themes/umbra/info.php
@@ -2,7 +2,7 @@
 return array(
     "name"        	=> __("Umbra", "umbra"),
     "url"         	=> "http://chyrplite.net/",
-    "version"     	=> 1.0,
+    "version"     	=> 1.1,
     "description" 	=> __("A dark tumbleblog theme for Chyrp Lite.", "umbra"),
     "author"      	=> array(
         "name"    	=> "Daniel Pimley",
