9f603347f51f4811f229733101c34fb12ce54c28
Daniel Pimley <dan@pimley.net>
2019-11-30 18:36:56 +0000

    Translation: removed unnecessary repeat unpack in load()

--- includes/class/Translation.php
+++ includes/class/Translation.php
@@ -64,10 +64,10 @@
                 return false;
 
             $unpack = ($big_endian) ?
-                "H8magic/Nformat/Nnum/Nor/Ntr" :
-                "H8magic/Vformat/Vnum/Vor/Vtr" ;
+                "Nformat/Nnum/Nor/Ntr" :
+                "Vformat/Vnum/Vor/Vtr" ;
 
-            $mo_offset = unpack($unpack, $mo_file);
+            $mo_offset = unpack($unpack, $mo_file, 4);
 
             $unpack = ($big_endian) ?
                 "Nlength/Noffset" :
