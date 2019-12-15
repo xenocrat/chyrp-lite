f1f77084b3d5e2029efd9b5a27304f73715ce95d
Daniel Pimley <dan@pimley.net>
2019-12-15 20:47:55 +0000

    Common: updated version and codename, removed license text

--- includes/common.php
+++ includes/common.php
@@ -1,35 +1,7 @@
 <?php
     /**
      * File: common
-     *
-     * BSD 3-Clause License
-     *
-     * Copyright 2008-2019 Alex Suraci, Arian Xhezairi, Daniel Pimley, and others.
-     *
-     * Redistribution and use in source and binary forms, with or without
-     * modification, are permitted provided that the following conditions are met:
-     *
-     * 1. Redistributions of source code must retain the above copyright notice, this
-     *    list of conditions and the following disclaimer.
-     *
-     * 2. Redistributions in binary form must reproduce the above copyright notice,
-     *    this list of conditions and the following disclaimer in the documentation
-     *    and/or other materials provided with the distribution.
-     *
-     * 3. Neither the name of the copyright holder nor the names of its
-     *    contributors may be used to endorse or promote products derived from
-     *    this software without specific prior written permission.
-     *
-     * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-     * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-     * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-     * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-     * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-     * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-     * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-     * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-     * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-     * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+     * Configures the Chyrp Lite environment.
      */
 
     # Constant: DEBUG
@@ -38,11 +10,11 @@
 
     # Constant: CHYRP_VERSION
     # Version number for this release.
-    define('CHYRP_VERSION', "2019.04");
+    define('CHYRP_VERSION', "2020.01");
 
     # Constant: CHYRP_CODENAME
     # The codename for this version.
-    define('CHYRP_CODENAME', "Arabian");
+    define('CHYRP_CODENAME', "Desert");
 
     # Constant: CHYRP_IDENTITY
     # The string identifying this version.
