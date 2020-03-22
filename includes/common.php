e8551d1ada0d6553994b84b1ec822f7d6ec1c436
Daniel Pimley <dan@pimley.net>
2020-03-22 13:11:06 +0000

    Updated version and codename

--- includes/common.php
+++ includes/common.php
@@ -10,11 +10,11 @@
 
     # Constant: CHYRP_VERSION
     # Version number for this release.
-    define('CHYRP_VERSION', "2020.01");
+    define('CHYRP_VERSION', "2020.02");
 
     # Constant: CHYRP_CODENAME
     # The codename for this version.
-    define('CHYRP_CODENAME', "Desert");
+    define('CHYRP_CODENAME', "Somali");
 
     # Constant: CHYRP_IDENTITY
     # The string identifying this version.
