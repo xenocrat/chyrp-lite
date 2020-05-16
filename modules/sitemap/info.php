8e45e3f7e53872c8bf57917f517cf308356a8559
Daniel Pimley <dan@pimley.net>
2020-05-16 12:40:30 +0100

    Sitemap module: removed notification when enabling

--- modules/sitemap/info.php
+++ modules/sitemap/info.php
@@ -6,7 +6,5 @@ return array(
     "description"   => __("Creates a <em>sitemap.xml</em> file on your server to help search engines index your blog.", "sitemap"),
     "author"        => array(
         "name"      => "Poenta Labs",
-        "url"       => "http://poentalabs.com/"),
-    "notifications" => array(
-                        __("Please remember to give write permissions to sitemap.xml on the root directory of the site.", "sitemap"))
+        "url"       => "http://poentalabs.com/")
 );
