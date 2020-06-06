03a9077d135ca8e76814f45312958c046ec4aee3
Daniel Pimley <dan@pimley.net>
2020-06-06 11:24:08 +0100

    Updated version and codename

--- tools/triggers.php
+++ tools/triggers.php
@@ -7,8 +7,8 @@
     header("Content-Type: text/html; charset=UTF-8");
 
     define('DEBUG',          true);
-    define('CHYRP_VERSION',  "2020.02");
-    define('CHYRP_CODENAME', "Somali");
+    define('CHYRP_VERSION',  "2020.03");
+    define('CHYRP_CODENAME', "Dead Sea");
     define('CHYRP_IDENTITY', "Chyrp/".CHYRP_VERSION." (".CHYRP_CODENAME.")");
     define('MAIN',           false);
     define('ADMIN',          false);
