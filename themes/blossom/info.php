7a78dd10ad2d29fcd7be2e547e9c38249435b870
Daniel Pimley <dan@pimley.net>
2020-07-18 15:52:06 +0100

    Blossom theme: stylesheet changes and version bump

--- themes/blossom/info.php
+++ themes/blossom/info.php
@@ -2,7 +2,7 @@
 return array(
     "name"          => __("Blossom", "blossom"),
     "url"           => "http://chyrplite.net/",
-    "version"       => 2.3,
+    "version"       => 2.4,
     "description"   => __("The default theme provided with Chyrp Lite.", "blossom"),
     "author"        => array(
         "name"      => "Daniel Pimley",
