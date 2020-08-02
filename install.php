dd7940ef3334f70445d0688a55f15793700abb54
Daniel Pimley <dan@pimley.net>
2020-08-02 16:16:09 +0100

    Updated version and codename

--- install.php
+++ install.php
@@ -7,8 +7,8 @@
     header("Content-Type: text/html; charset=UTF-8");
 
     define('DEBUG',            true);
-    define('CHYRP_VERSION',    "2020.03");
-    define('CHYRP_CODENAME',   "Dead Sea");
+    define('CHYRP_VERSION',    "2020.04");
+    define('CHYRP_CODENAME',   "Spanish");
     define('CHYRP_IDENTITY',   "Chyrp/".CHYRP_VERSION." (".CHYRP_CODENAME.")");
     define('MAIN',             false);
     define('ADMIN',            false);
