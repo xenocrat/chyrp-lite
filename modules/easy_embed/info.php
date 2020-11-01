a4d3cc7ff6d4a6395aa738bc6e244e3df38d73f5
Daniel Pimley <dan@pimley.net>
2020-11-01 13:04:04 +0000

    Easy Embed module: removed defunct services

--- modules/easy_embed/info.php
+++ modules/easy_embed/info.php
@@ -2,7 +2,7 @@
 return array(
     "name"          => __("Easy Embed", "easy_embed"),
     "url"           => "http://chyrplite.net/",
-    "version"       => 1.0,
+    "version"       => 1.1,
     "description"   => __("Embed a video in your blog content by pasting its URL surrounded by <code>&lt;!--</code> and <code>--&gt;</code>.", "easy_embed"),
     "author"        => array(
         "name"      => "Daniel Pimley",
