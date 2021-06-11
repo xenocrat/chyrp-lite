25ca788622a7a677a1d3e5c2a69f89ade489c9ba
Daniel Pimley <dan@pimley.net>
2021-06-11 20:34:35 +0100

    Blog themes: improved accessibility

--- themes/topaz/javascripts/theme.js
+++ themes/topaz/javascripts/theme.js
@@ -5,18 +5,18 @@ $(document).ready(function() {
     $("#mobile_toggle_link").on("click", function(e) {
         e.preventDefault();
 
-        if ($("ul.tail_nav.mobile_nav").hasClass("on")) {
-            $("ul.tail_nav.mobile_nav").removeClass("on");
+        if ($("ul#nav.mobile_nav").hasClass("on")) {
+            $("ul#nav.mobile_nav").removeClass("on");
         } else {
-            $("ul.tail_nav.mobile_nav").addClass("on");
+            $("ul#nav.mobile_nav").addClass("on");
         }
     });
 
-    $("ul.tail_nav").addClass("mobile_nav");
+    $("ul#nav").addClass("mobile_nav");
     $("body").addClass("mobile_nav");
 
     // Make the menu items keyboard accessible.
-    $("ul.tail_nav.mobile_nav").on("focus", "a:not(#mobile_toggle_link)", function() {
-        $("ul.tail_nav.mobile_nav").addClass("on");
+    $("ul#nav.mobile_nav").on("focus", "a:not(#mobile_toggle_link)", function() {
+        $("ul#nav.mobile_nav").addClass("on");
     });
 });
