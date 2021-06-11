8f01fcc43d24b35e8f14866b2ae9e760f892e4b3
Daniel Pimley <dan@pimley.net>
2021-06-11 23:05:00 +0100

    Blog themes: more breaking changes and a version bump

--- themes/blossom/info.php
+++ themes/blossom/info.php
@@ -2,7 +2,7 @@
 return array(
     "name"          => __("Blossom", "blossom"),
     "url"           => "http://chyrplite.net/",
-    "version"       => 2.5,
+    "version"       => 2.6,
     "description"   => __("The default theme provided with Chyrp Lite.", "blossom"),
     "author"        => array(
         "name"      => "Daniel Pimley",
--- themes/sparrow/info.php
+++ themes/sparrow/info.php
@@ -2,7 +2,7 @@
 return array(
     "name"        	=> __("Sparrow", "sparrow"),
     "url"         	=> "http://chyrplite.net/",
-    "version"     	=> 2.2,
+    "version"     	=> 2.3,
     "description" 	=> __("An unobtrusive tumbleblog theme for Chyrp Lite.", "sparrow"),
     "author"      	=> array(
         "name"    	=> "Daniel Pimley",
