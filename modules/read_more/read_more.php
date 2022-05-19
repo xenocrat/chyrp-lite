14e4eb7193f5f348ba77e38be5d188beab09c397
Daniel Pimley <dan@pimley.net>
2022-05-19 23:07:59 +0100

    Read More module: added setting to apply to feeds
    See #156.

--- modules/read_more/read_more.php
+++ modules/read_more/read_more.php
@@ -1,8 +1,42 @@
 <?php
     class ReadMore extends Modules {
+        static function __install(): void {
+            Config::current()->set("module_read_more", array("apply_to_feeds" => false));
+        }
+
+        static function __uninstall(): void {
+            Config::current()->remove("module_read_more");
+        }
+
         public function __init(): void {
             # Truncate in "markup_post_text" before Markdown filtering in "markup_text".
             $this->setPriority("markup_post_text", 1);
+        }
+
+        public function admin_read_more_settings($admin): void {
+            if (!Visitor::current()->group->can("change_settings"))
+                show_403(__("Access Denied"),
+                         __("You do not have sufficient privileges to change settings."));
+    
+            if (empty($_POST)) {
+                $admin->display("pages".DIR."read_more_settings");
+                return;
+            }
+    
+            if (!isset($_POST['hash']) or !authenticate($_POST['hash']))
+                show_403(__("Access Denied"), __("Invalid authentication token."));
+    
+            Config::current()->set("module_read_more",
+                                   array("apply_to_feeds" => isset($_POST['apply_to_feeds'])));
+
+            Flash::notice(__("Settings updated."), "read_more_settings");
+        }
+
+        public function settings_nav($navs): array {
+            if (Visitor::current()->group->can("change_settings"))
+                $navs["read_more_settings"] = array("title" => __("Read More", "read_more"));
+
+            return $navs;
         }
 
         public function markup_post_text($text, $post = null): string {
@@ -10,13 +44,10 @@
                 return $text;
 
             $route = Route::current();
+            $array = Config::current()->module_read_more;
 
-            if (!isset($post) or
-                !isset($route) or
-                !($route->controller instanceof MainController) or
-                $route->controller->feed or
-                $route->action == "view")
-                    return preg_replace("/<!-- *more([^>]*)?-->/i", "", $text);
+            if (!isset($post) or !$this->eligible())
+                return preg_replace("/<!-- *more([^>]*)?-->/i", "", $text);
 
             $more = oneof(trim(fallback($matches[1])), __("&hellip;more", "read_more"));
             $url = (!$post->no_results) ? $post->url() : "#" ;
@@ -28,5 +59,24 @@
         public function title_from_excerpt($text): string {
             $split = preg_split('/<a class="read_more"/', $text);
             return $split[0];
+        }
+
+        private function eligible(): bool {
+            $route = Route::current();
+            $array = Config::current()->module_read_more;
+
+            if (!isset($route))
+                return false;
+
+            if (!$route->controller instanceof MainController)
+                return false;
+
+            if ($route->action == "view")
+                return false;
+
+            if ($route->controller->feed and !$array["apply_to_feeds"])
+                return false;
+
+            return true;
         }
     }
---
+++ modules/read_more/upgrades.php
@@ -0,0 +1,18 @@
+<?php
+    if (!defined('UPGRADING') or !UPGRADING)
+        exit;
+
+    /**
+     * Function: read_more_add_config
+     * Adds config settings into an array.
+     *
+     * Versions: 2022.01 => 2022.02
+     */
+    function read_more_add_config(): void {
+        $set = Config::current()->set("module_read_more", array("apply_to_feeds" => false), true);
+
+        if ($set === false)
+            error(__("Error"), __("Could not write the configuration file."));
+    }
+
+    read_more_add_config();
