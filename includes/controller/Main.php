a53b7e54f8bb5ccc3a33783ef357f688a5407718
Daniel Pimley <dan@pimley.net>
2022-08-12 17:29:54 +0100

    MainController: fixed search edge case

--- includes/controller/Main.php
+++ includes/controller/Main.php
@@ -291,7 +291,7 @@
             if (isset($_POST['query']))
                 redirect("search/".str_ireplace("%2F", "", urlencode($_POST['query']))."/");
 
-            if (empty($_GET['query']))
+            if (!isset($_GET['query']) or $_GET['query'] == "")
                 Flash::warning(__("Please enter a search term."), "/");
 
             list($where, $params) = keywords($_GET['query'],
