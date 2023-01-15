289234acb915ac01209c659578306610f1e09cb9
Daniel Pimley <dan@pimley.net>
2023-01-15 19:07:34 +0000

    Lots of CSS tweaks

--- tools/gettext.php
+++ tools/gettext.php
@@ -606,7 +606,7 @@
             }
             pre {
                 font-family: "Cousine webfont", monospace;
-                font-size: 0.9em;
+                font-size: 0.85em;
                 background-color: #efefef;
                 margin: 1rem 0rem;
                 padding: 1rem;
@@ -614,14 +614,14 @@
             }
             code {
                 font-family: "Cousine webfont", monospace;
-                font-size: 0.9em;
+                font-size: 0.85em;
                 background-color: #efefef;
                 padding: 0px 2px;
                 border: 1px solid #cfcfcf;
                 vertical-align: bottom;
             }
             pre > code {
-                font-size: 0.9rem;
+                font-size: 0.85rem;
                 display: block;
                 border: none;
                 padding: 0px;
