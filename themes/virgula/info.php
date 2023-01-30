308aeaf3452eadb17132b64086011d6810f047d0
Daniel Pimley <dan@pimley.net>
2023-01-30 15:31:57 +0000

    Added new theme: Virgula

---
+++ themes/virgula/info.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    "name"          => __("Virgula", "virgula"),
+    "url"           => "http://chyrplite.net/",
+    "version"       => "2023.01",
+    "description"   => __("A high contrast theme for Chyrp Lite.", "virgula"),
+    "author"        => array(
+        "name"      => "Daniel Pimley",
+        "url"       => "http://pimley.net/"
+    )
+);
