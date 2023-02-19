db144936e4c280e34667e51a4b13a3daa6cda6d5
Daniel Pimley <dan@pimley.net>
2023-02-19 18:40:03 +0000

    Core: improved underline visibility

--- upgrade.php
+++ upgrade.php
@@ -713,6 +713,7 @@
             a:visited {
                 color: #1f1f23;
                 text-decoration: underline;
+                text-decoration-thickness: 1px;
             }
             a:focus {
                 outline: #ff7f00 dashed 2px;
@@ -723,6 +724,7 @@
             a:active {
                 color: #1e57ba;
                 text-decoration: underline;
+                text-decoration-thickness: 1px;
             }
             a.big,
             button {
