b7b9b63db7bebc9f887fce04b9c9d865db30ae1f
Daniel Pimley <dan@pimley.net>
2023-05-12 15:41:51 +0100

    Modules: updated info.php files

--- modules/comments/info.php
+++ modules/comments/info.php
@@ -2,7 +2,7 @@
 return array(
     "name"          => __("Comments", "comments"),
     "url"           => "http://chyrplite.net/",
-    "version"       => "2023.01",
+    "version"       => "2023.02",
     "description"   => __("Adds commenting functionality to your posts, with webmention support.", "comments"),
     "author"        => array(
         "name"      => "Chyrp Team",
