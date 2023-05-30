ca07318f8c48a85fcff5a371254a5967914b0347
Daniel Pimley <dan@pimley.net>
2023-05-30 20:12:02 +0100

    Blog themes: more spring cleaning
    Renamed mobil_nav to js_nav so its function is more obvious.

--- themes/topaz/javascripts/theme.js
+++ themes/topaz/javascripts/theme.js
@@ -5,18 +5,18 @@ $(document).ready(function() {
     $("#mobile_toggle_link").on("click", function(e) {
         e.preventDefault();
 
-        if ($("ul#nav.mobile_nav").hasClass("on")) {
-            $("ul#nav.mobile_nav").removeClass("on");
+        if ($("ul#nav.js_nav").hasClass("on")) {
+            $("ul#nav.js_nav").removeClass("on");
         } else {
-            $("ul#nav.mobile_nav").addClass("on");
+            $("ul#nav.js_nav").addClass("on");
         }
     });
 
-    $("ul#nav").addClass("mobile_nav");
-    $("body").addClass("mobile_nav");
+    $("ul#nav").addClass("js_nav");
+    $("body").addClass("js_nav");
 
     // Make the menu items keyboard accessible.
-    $("ul#nav.mobile_nav").on("focus", "a:not(#mobile_toggle_link)", function() {
-        $("ul#nav.mobile_nav").addClass("on");
+    $("ul#nav.js_nav").on("focus", "a:not(#mobile_toggle_link)", function() {
+        $("ul#nav.js_nav").addClass("on");
     });
 });
