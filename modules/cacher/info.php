3160ab6c5152497989b6fc22ddf87ed0589dc429
Daniel Pimley <dan@pimley.net>
2023-09-28 12:21:05 +0100

    Cacher module: added regen trigger delete_upload
    Fixes #207.

--- modules/cacher/info.php
+++ modules/cacher/info.php
@@ -2,7 +2,7 @@
 return array(
     "name"          => __("Cacher", "cacher"),
     "url"           => "http://chyrplite.net/",
-    "version"       => "2023.02",
+    "version"       => "2023.03",
     "description"   => __("Caches pages, drastically reducing server load.", "cacher"),
     "author"        => array(
         "name"      => "Daniel Pimley",
