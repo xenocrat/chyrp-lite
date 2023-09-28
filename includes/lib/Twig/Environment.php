4ff41e40f9697a1588ad14cb9aaae686e82362e9
Daniel Pimley <dan@pimley.net>
2023-09-28 20:03:21 +0100

    Updated Twig to 3.7.1

--- includes/lib/Twig/Environment.php
+++ includes/lib/Twig/Environment.php
@@ -40,10 +40,10 @@ use Twig\TokenParser\TokenParserInterface;
  */
 class Environment
 {
-    public const VERSION = '3.6.1';
-    public const VERSION_ID = 30601;
+    public const VERSION = '3.7.1';
+    public const VERSION_ID = 30701;
     public const MAJOR_VERSION = 3;
-    public const MINOR_VERSION = 6;
+    public const MINOR_VERSION = 7;
     public const RELEASE_VERSION = 1;
     public const EXTRA_VERSION = '';
 
