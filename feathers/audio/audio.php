2857b8a1ce5ab198ac9d76a8cfce0f5a32d9e5e6
Daniel Pimley <dan@pimley.net>
2023-12-23 12:25:18 +0000

    Audio feather: added VTT caption support

--- feathers/audio/audio.php
+++ feathers/audio/audio.php
@@ -23,6 +23,17 @@
             );
             $this->setField(
                 array(
+                    "attr" => "captions",
+                    "type" => "file",
+                    "label" => __("Captions", "audio"),
+                    "optional" => true,
+                    "multiple" => false,
+                    "accept" => ".vtt",
+                    "note" => _f("(Max. file size: %d Megabytes)", $maximum, "audio")
+                )
+            );
+            $this->setField(
+                array(
                     "attr" => "description",
                     "type" => "text_block",
                     "label" => __("Description", "audio"),
@@ -56,6 +67,12 @@
                     code:422
                 );
 
+            if (isset($_FILES['captions']) and upload_tester($_FILES['captions']))
+                $captions = upload(
+                    $_FILES['captions'],
+                    array(".vtt")
+                );
+
             fallback($_POST['title'], "");
             fallback($_POST['description'], "");
             fallback($_POST['slug'], $_POST['title']);
@@ -67,6 +84,7 @@
                 values:array(
                     "title" => $_POST['title'],
                     "filename" => $filename,
+                    "captions" => fallback($captions, ""),
                     "description" => $_POST['description']
                 ),
                 clean:sanitize($_POST['slug']),
@@ -87,6 +105,7 @@
             fallback($_POST['created_at'], $post->created_at);
             fallback($_POST['option'], array());
             $filename = $post->filename;
+            $captions = $post->captions;
 
             if (isset($_FILES['filename']) and upload_tester($_FILES['filename']))
                 $filename = upload(
@@ -98,6 +117,7 @@
                 values:array(
                     "title" => $_POST['title'],
                     "filename" => $filename,
+                    "captions" => fallback($captions, ""),
                     "description" => $_POST['description']
                 ),
                 pinned:!empty($_POST['pinned']),
@@ -124,17 +144,29 @@
         }
 
         public function enclose_audio($post, $feed) {
-            $config = Config::current();
-            $filepath = uploaded($post->filename, false);
-
-            if ($post->feather != "audio" or !file_exists($filepath))
+            if ($post->feather != "audio")
                 return;
 
-            $feed->enclosure(
-                uploaded($post->filename),
-                filesize($filepath),
-                $this->audio_type($post->filename)
-            );
+            $filepath = uploaded($post->filename, false);
+
+            if (file_exists($filepath))
+                $feed->enclosure(
+                    uploaded($post->filename),
+                    filesize($filepath),
+                    $this->audio_type($post->filename)
+                );
+
+            if (empty($post->captions))
+                return;
+
+            $filepath = uploaded($post->captions, false);
+
+            if (file_exists($filepath))
+                $feed->enclosure(
+                    uploaded($post->captions),
+                    filesize($filepath),
+                    $this->audio_type($post->captions)
+                );
         }
 
         public function filter_post($post): void {
@@ -150,20 +182,30 @@
             if ($trigger->exists("audio_player"))
                 return $trigger->call("audio_player", $post);
 
-            return '<audio controls>'.
-                   "\n".
-                   __("Your web browser does not support the <code>audio</code> element.", "audio").
-                   "\n".
-                   '<source src="'.uploaded($post->filename).
-                   '" type="'.$this->audio_type($post->filename).
-                   '">'.
-                   "\n".
-                   '</audio>'.
-                   "\n";
+            $player = '<audio controls>'.
+                      "\n".
+                      __("Your web browser does not support the <code>audio</code> element.", "audio").
+                      "\n".
+                      '<source src="'.
+                      uploaded($post->filename).
+                      '" type="'.
+                      $this->audio_type($post->filename).
+                      '">'.
+                      "\n";
+
+            if (!empty($post->captions))
+                $player.= '<track kind="captions" src="'.
+                          uploaded($post->captions).'">'."\n";
+
+            $player.= '</audio>'."\n";
+
+            return $player;
         }
 
         private function audio_type($filename): string {
-            $extension = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
+            $extension = strtolower(
+                pathinfo($filename, PATHINFO_EXTENSION)
+            );
 
             switch($extension) {
                 case "mp3":
--- feathers/audio/info.php
+++ feathers/audio/info.php
@@ -2,7 +2,7 @@
 return array(
     "name"          => __("Audio", "audio"),
     "url"           => "http://chyrplite.net/",
-    "version"       => "2023.01",
+    "version"       => "2024.01",
     "description"   => __("A feather for audio.", "audio"),
     "author"        => array(
         "name"      => "Daniel Pimley",
