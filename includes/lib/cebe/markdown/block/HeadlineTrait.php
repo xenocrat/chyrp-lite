42bf79ec62bc9816baafa712e2423c5338a68e49
Daniel Pimley <dan@pimley.net>
2024-02-20 10:55:17 +0000

    cebe/markdown: CM compat in HeadlineTrait
    Requiring a space after the hashes, but still ignoring leading spaces for speed of implementation.

--- includes/lib/cebe/markdown/block/HeadlineTrait.php
+++ includes/lib/cebe/markdown/block/HeadlineTrait.php
@@ -19,7 +19,7 @@ trait HeadlineTrait
 	{
 		return (
 			// heading with #
-			$line[0] === '#' && !preg_match('/^#\d+/', $line)
+			$line[0] === '#' && preg_match('/^#{1,6}[ \t]/', $line)
 			||
 			// underlined headline
 			!empty($lines[$current + 1]) &&
