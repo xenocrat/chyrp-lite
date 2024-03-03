8010927f6bbe0178a2f6c20f54a87b6be037636f
Daniel Pimley <dan@pimley.net>
2024-03-03 20:15:09 +0000

    CHYRP_VERSION: 2024.01.01

--- tools/gettext.php
+++ tools/gettext.php
@@ -7,7 +7,7 @@
     header("Content-Type: text/html; charset=UTF-8");
 
     define('DEBUG',                         true);
-    define('CHYRP_VERSION',                 "2024.01");
+    define('CHYRP_VERSION',                 "2024.01.01");
     define('CHYRP_CODENAME',                "Sombre");
     define('CHYRP_IDENTITY',                "Chyrp/".CHYRP_VERSION." (".CHYRP_CODENAME.")");
     define('MAIN',                          false);
