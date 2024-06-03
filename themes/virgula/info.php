2e8496b59b1e62d6e90ddf70c75b697cb87c7795
Daniel Pimley <dan@pimley.net>
2024-06-03 10:06:10 +0100

    Virgula theme: stylesheet tweak and version bump

--- themes/virgula/info.php
+++ themes/virgula/info.php
@@ -2,7 +2,7 @@
 return array(
     "name"          => __("Virgula", "virgula"),
     "url"           => "http://chyrplite.net/",
-    "version"       => "2024.02",
+    "version"       => "2024.03",
     "description"   => __("A high-contrast theme for Chyrp Lite.", "virgula"),
     "author"        => array(
         "name"      => "Daniel Pimley",
