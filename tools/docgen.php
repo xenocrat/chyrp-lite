4c66328bc9d96c68d49d8c3357c6811f4a85a360
Daniel Pimley <dan@pimley.net>
2024-07-27 11:14:13 +0100

    Tools: added constant SLUG_STRICT

--- tools/docgen.php
+++ tools/docgen.php
@@ -39,6 +39,7 @@
     define('UPDATE_INTERVAL',               null);
     define('UPDATE_PAGE',                   null);
     define('SESSION_DENY_BOT',              true);
+    define('SLUG_STRICT',                   true);
     define('GET_REMOTE_UNSAFE',             false);
     define('USE_GETTEXT_SHIM',              stripos(PHP_OS, "Win") === 0);
     define('USE_OB',                        true);
