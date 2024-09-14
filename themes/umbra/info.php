842158a22899a9341d5272e30c5f8eb9936594be
Daniel Pimley <dan@pimley.net>
2024-09-14 13:25:27 +0100

    Blog themes: styling tweaks

--- themes/umbra/info.php
+++ themes/umbra/info.php
@@ -2,7 +2,7 @@
 return array(
     "name"          => __("Umbra", "umbra"),
     "url"           => "http://chyrplite.net/",
-    "version"       => "2024.03",
+    "version"       => "2025.01",
     "description"   => __("A dark tumbleblog theme for Chyrp Lite.", "umbra"),
     "author"        => array(
         "name"      => "Daniel Pimley",
--- themes/virgula/info.php
+++ themes/virgula/info.php
@@ -2,7 +2,7 @@
 return array(
     "name"          => __("Virgula", "virgula"),
     "url"           => "http://chyrplite.net/",
-    "version"       => "2024.03",
+    "version"       => "2025.01",
     "description"   => __("A high-contrast theme for Chyrp Lite.", "virgula"),
     "author"        => array(
         "name"      => "Daniel Pimley",
