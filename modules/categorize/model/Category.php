bf2b863a08f870f0382f3645fc40c875404dc1de
Daniel Pimley <dan@pimley.net>
2024-11-23 12:11:54 +0000

    Categorize module: change to Category->update()

--- modules/categorize/model/Category.php
+++ modules/categorize/model/Category.php
@@ -93,7 +93,7 @@
         public function update(
             $name,
             $clean = null,
-            $show_on_home = true
+            $show_on_home = null
         ): self|false {
             if ($this->no_results)
                 return false;
