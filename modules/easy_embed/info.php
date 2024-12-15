6b7d85f1fadcfa969c1973f3ef754fce296fc277
Daniel Pimley <dan@pimley.net>
2024-12-15 16:49:06 +0000

    Easy Embed module: updated info.php

--- modules/easy_embed/info.php
+++ modules/easy_embed/info.php
@@ -2,8 +2,8 @@
 return array(
     "name"          => __("Easy Embed", "easy_embed"),
     "url"           => "http://chyrplite.net/",
-    "version"       => "2023.01",
-    "description"   => __("Embed a video in your blog content by pasting its URL surrounded by <code>&lt;!--</code> and <code>--&gt;</code>.", "easy_embed"),
+    "version"       => "2025.01",
+    "description"   => __("Embed content in your blog by pasting its URL surrounded by <code>&lt;!--</code> and <code>--&gt;</code>.", "easy_embed"),
     "author"        => array(
         "name"      => "Daniel Pimley",
         "url"       => "http://www.pimley.net/"
