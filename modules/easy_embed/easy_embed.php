2483bc86e2acbe7c1dfb9ad00838f89495c03d6d
Daniel Pimley <dan@pimley.net>
2024-12-15 16:36:13 +0000

    Easy Embed module: improved accessibility

--- modules/easy_embed/easy_embed.php
+++ modules/easy_embed/easy_embed.php
@@ -11,26 +11,40 @@
         ): string {
             $urls = array(
                 '|<!--[^>]*youtube.com/watch\?v=([a-z0-9_\-]{11})[^>]*-->|i'
-                    => 'https://www.youtube.com/embed/$1',
+                    => '<iframe class="content_embed youtube_embed" src="'.
+                       'https://www.youtube.com/embed/$1"'.
+                       ' title="'.
+                       __("Embedded content from YouTube", "easy_embed").
+                       '" allowfullscreen loading="lazy"></iframe>',
 
                 '|<!--[^>]*youtu.be/([a-z0-9_\-]{11})[^>]*-->|i'
-                    => 'https://www.youtube.com/embed/$1',
+                    => '<iframe class="content_embed youtube_embed" src="'.
+                       'https://www.youtube.com/embed/$1"'.
+                       ' title="'.
+                       __("Embedded content from YouTube", "easy_embed").
+                       '" allowfullscreen loading="lazy"></iframe>',
 
                 '|<!--[^>]*vimeo.com/([0-9]{9})[^>]*-->|i'
-                    => 'https://player.vimeo.com/video/$1',
+                    => '<iframe class="content_embed vimeo_embed" src="'.
+                       'https://player.vimeo.com/video/$1"'.
+                       ' title="'.
+                       __("Embedded content from Vimeo", "easy_embed").
+                       '" allowfullscreen loading="lazy"></iframe>',
 
                 '|<!--[^>]*twitch.tv/[^/]+/v/([0-9]{9})[^>]*-->|i'
-                    => 'https://player.twitch.tv/?video=v$1',
+                    => '<iframe class="content_embed twitch_embed" src="'.
+                       'https://player.twitch.tv/?video=v$1"'.
+                       ' title="'.
+                       __("Embedded content from Twitch", "easy_embed").
+                       '" allowfullscreen loading="lazy"></iframe>',
 
                 '|<!--[^>]*archive.org/details/([a-z0-9_\-]+)[^>]*-->|i'
-                    => 'https://archive.org/embed/$1'
+                    => '<iframe class="content_embed archiveorg_embed" src="'.
+                       'https://archive.org/embed/$1"'.
+                       ' title="'.
+                       __("Embedded content from Internet Archive", "easy_embed").
+                       '" allowfullscreen loading="lazy"></iframe>'
             );
-
-            foreach ($urls as $view => &$embed) {
-                $embed = '<iframe class="video_embed" src="'.
-                         fix($embed, true).
-                         '" allowfullscreen></iframe>';
-            }
 
             return preg_replace(
                 array_keys($urls),
