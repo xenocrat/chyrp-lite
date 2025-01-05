f280817d9d57f21a54a4842424ebd11cfa594641
Daniel Pimley <dan@pimley.net>
2025-01-05 20:37:56 +0000

    Easy Embed module: improved support

--- modules/easy_embed/easy_embed.php
+++ modules/easy_embed/easy_embed.php
@@ -10,8 +10,13 @@
             $text
         ): string {
             $urls = array(
-
                 # YouTube:
+                    '|<!--[^>]*youtube.com/embed/([a-z0-9_\-]{11})[^>]*-->|i'
+                =>  '<iframe class="content_embed youtube_embed" src="'.
+                    'https://www.youtube.com/embed/$1"'.
+                    ' title="'.
+                    __("Embedded content from YouTube", "easy_embed").
+                    '" allowfullscreen loading="lazy"></iframe>',
 
                     '|<!--[^>]*youtube.com/watch\?v=([a-z0-9_\-]{11})[^>]*-->|i'
                 =>  '<iframe class="content_embed youtube_embed" src="'.
@@ -28,6 +33,12 @@
                     '" allowfullscreen loading="lazy"></iframe>',
 
                 # Vimeo:
+                    '|<!--[^>]*player.vimeo.com/video/([0-9]{9})[^>]*-->|i'
+                =>  '<iframe class="content_embed vimeo_embed" src="'.
+                    'https://player.vimeo.com/video/$1"'.
+                    ' title="'.
+                    __("Embedded content from Vimeo", "easy_embed").
+                    '" allowfullscreen loading="lazy"></iframe>',
 
                     '|<!--[^>]*vimeo.com/([0-9]{9})[^>]*-->|i'
                 =>  '<iframe class="content_embed vimeo_embed" src="'.
@@ -37,6 +48,12 @@
                     '" allowfullscreen loading="lazy"></iframe>',
 
                 # Twitch:
+                    '|<!--[^>]*player.twitch.tv/\?video=v([0-9]{9})[^>]*-->|i'
+                =>  '<iframe class="content_embed twitch_embed" src="'.
+                    'https://player.twitch.tv/?video=v$1"'.
+                    ' title="'.
+                    __("Embedded content from Twitch", "easy_embed").
+                    '" allowfullscreen loading="lazy"></iframe>',
 
                     '|<!--[^>]*twitch.tv/[^/]+/v/([0-9]{9})[^>]*-->|i'
                 =>  '<iframe class="content_embed twitch_embed" src="'.
@@ -46,6 +63,12 @@
                     '" allowfullscreen loading="lazy"></iframe>',
 
                 # Internet Archive:
+                    '|<!--[^>]*archive.org/embed/([a-z0-9_\-]+)[^>]*-->|i'
+                =>  '<iframe class="content_embed archiveorg_embed" src="'.
+                    'https://archive.org/embed/$1"'.
+                    ' title="'.
+                    __("Embedded content from Internet Archive", "easy_embed").
+                    '" allowfullscreen loading="lazy"></iframe>',
 
                     '|<!--[^>]*archive.org/details/([a-z0-9_\-]+)[^>]*-->|i'
                 =>  '<iframe class="content_embed archiveorg_embed" src="'.
@@ -55,33 +78,31 @@
                     '" allowfullscreen loading="lazy"></iframe>',
 
                 # Spotify:
-
-                    '|<!--[^>]*open.spotify.com/embed/show/([a-z0-9]+)[^>]*-->|i'
+                    '|<!--[^>]*open.spotify.com/embed/([a-z0-9/]+)[^>]*-->|i'
                 =>  '<iframe class="content_embed spotify_embed" src="'.
-                    'https://open.spotify.com/embed/show/$1"'.
+                    'https://open.spotify.com/embed/$1"'.
                     ' title="'.
                     __("Embedded content from Spotify", "easy_embed").
                     '" allowfullscreen loading="lazy"></iframe>',
 
                 # Bandcamp:
-
-                    '|<!--[^>]*bandcamp.com/EmbeddedPlayer([a-z0-9/=]+)(minimal=true)([a-z0-9/=]+)[^>]*-->|i'
+                    '|<!--[^>]*bandcamp.com/EmbeddedPlayer/([a-z0-9/=]*minimal=true[a-z0-9/=]*)[^>]*-->|i'
                 =>  '<iframe class="content_embed bandcamp_m_embed" src="'.
-                    'https://bandcamp.com/EmbeddedPlayer$1$2$3"'.
+                    'https://bandcamp.com/EmbeddedPlayer/$1"'.
                     ' title="'.
                     __("Embedded content from Bandcamp", "easy_embed").
                     '" allowfullscreen loading="lazy"></iframe>',
 
-                    '|<!--[^>]*bandcamp.com/EmbeddedPlayer([a-z0-9/=]+)(size=small)([a-z0-9/=]+)[^>]*-->|i'
+                    '|<!--[^>]*bandcamp.com/EmbeddedPlayer/([a-z0-9/=]*size=small[a-z0-9/=]*)[^>]*-->|i'
                 =>  '<iframe class="content_embed bandcamp_s_embed" src="'.
-                    'https://bandcamp.com/EmbeddedPlayer$1$2$3"'.
+                    'https://bandcamp.com/EmbeddedPlayer/$1"'.
                     ' title="'.
                     __("Embedded content from Bandcamp", "easy_embed").
                     '" allowfullscreen loading="lazy"></iframe>',
 
-                    '|<!--[^>]*bandcamp.com/EmbeddedPlayer([a-z0-9/=]+)(size=large)([a-z0-9/=]+)[^>]*-->|i'
+                    '|<!--[^>]*bandcamp.com/EmbeddedPlayer/([a-z0-9/=]*size=large[a-z0-9/=]*)[^>]*-->|i'
                 =>  '<iframe class="content_embed bandcamp_l_embed" src="'.
-                    'https://bandcamp.com/EmbeddedPlayer$1$2$3"'.
+                    'https://bandcamp.com/EmbeddedPlayer/$1"'.
                     ' title="'.
                     __("Embedded content from Bandcamp", "easy_embed").
                     '" allowfullscreen loading="lazy"></iframe>'
