4edcb0b0f04be25afb540413ff5b0ee38f6787a6
Daniel Pimley <dan@pimley.net>
2025-01-26 13:21:04 +0000

    Bumped version and codename

--- includes/common.php
+++ includes/common.php
@@ -10,11 +10,11 @@
 
     # Constant: CHYRP_VERSION
     # Version number for this release.
-    define('CHYRP_VERSION', "2025.01");
+    define('CHYRP_VERSION', "2025.02");
 
     # Constant: CHYRP_CODENAME
     # The codename for this version.
-    define('CHYRP_CODENAME', "Boreal");
+    define('CHYRP_CODENAME', "Acacia");
 
     # Constant: CHYRP_IDENTITY
     # The string identifying this version.
--- tools/gettext.php
+++ tools/gettext.php
@@ -7,8 +7,8 @@
     header("Content-Type: text/html; charset=UTF-8");
 
     define('DEBUG',                         true);
-    define('CHYRP_VERSION',                 "2025.01");
-    define('CHYRP_CODENAME',                "Boreal");
+    define('CHYRP_VERSION',                 "2025.02");
+    define('CHYRP_CODENAME',                "Acacia");
     define('CHYRP_IDENTITY',                "Chyrp/".CHYRP_VERSION." (".CHYRP_CODENAME.")");
     define('MAIN',                          false);
     define('ADMIN',                         false);
