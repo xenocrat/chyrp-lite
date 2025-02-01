09d07a7700916ba0263b2f88fb830eee7f6e14f8
Daniel Pimley <dan@pimley.net>
2025-02-01 14:00:40 +0000

    Added lighttpd.conf.php

---
+++ includes/lighttpd.conf.php
@@ -0,0 +1,11 @@
+<?php header("Status: 403"); exit("Access denied."); ?>
+# Template to enable clean URLs for the Lighttpd web server.
+
+url.rewrite-once = (
+    "\.twig$" => "/{chyrp_path}/index.php"
+)
+
+url.rewrite-if-not-file = (
+    "^/{chyrp_path}/admin/.*" => "/{chyrp_path}/admin/index.php",
+    "^/{chyrp_path}/.*" => "/{chyrp_path}/index.php"
+)
