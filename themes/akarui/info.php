cfa33bed7e5e7c8f63b22e581df73df7b30af06a
Daniel Pimley <dan@pimley.net>
2025-12-22 19:56:23 +0000

    Akarui theme: A cheerful theme for Chyrp Lite

---
+++ themes/akarui/info.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    "name"          => __("Akarui", "akarui"),
+    "url"           => "http://chyrplite.net/",
+    "version"       => "2026.01",
+    "description"   => __("A cheerful theme for Chyrp Lite.", "akarui"),
+    "author"        => array(
+        "name"      => "Daniel Pimley",
+        "url"       => "http://pimley.net/"
+    )
+);
