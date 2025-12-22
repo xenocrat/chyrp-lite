22fa29a5e098659f9da19b8f249816b1e1420af1
Daniel Pimley <dan@pimley.net>
2025-12-22 19:52:45 +0000

    Topaz theme: changes to summary

--- themes/topaz/info.php
+++ themes/topaz/info.php
@@ -2,7 +2,7 @@
 return array(
     "name"          => __("Topaz", "topaz"),
     "url"           => "http://chyrplite.net/",
-    "version"       => "2025.03",
+    "version"       => "2026.01",
     "description"   => __("A minimalist responsive theme for Chyrp Lite.", "topaz"),
     "author"        => array(
         "name"      => "Daniel Pimley",
