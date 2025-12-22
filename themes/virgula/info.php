761170f7535a47d9c70bc1c70350c313fce2a9a9
Daniel Pimley <dan@pimley.net>
2025-12-22 19:52:31 +0000

    Virgula theme: changes to summary

--- themes/virgula/info.php
+++ themes/virgula/info.php
@@ -2,7 +2,7 @@
 return array(
     "name"          => __("Virgula", "virgula"),
     "url"           => "http://chyrplite.net/",
-    "version"       => "2025.03",
+    "version"       => "2026.01",
     "description"   => __("A high-contrast theme for Chyrp Lite.", "virgula"),
     "author"        => array(
         "name"      => "Daniel Pimley",
