d6c7125381db0cced4cb9563ebf66c19680b5475
Daniel Pimley <dan@pimley.net>
2026-02-04 21:35:30 +0000

    Bundled xenocrat/chyrp-csp

---
+++ modules/content_security_policy/content_security_policy.php
@@ -0,0 +1,36 @@
+<?php
+    class ContentSecurityPolicy extends Modules {
+        private $nonce;
+
+        public function runtime(
+        ): void {
+            $this->nonce = base64_encode(
+                random(32)
+            );
+
+            $this->hash = base64_encode(
+                hash("sha256", error_style(), true)
+            );
+
+            header(
+                "Content-Security-Policy:".
+                " default-src 'self';".
+                " style-src 'self' 'nonce-".$this->nonce."' 'sha256-".$this->hash."';".
+                " script-src 'self' 'nonce-".$this->nonce."';".
+                " frame-ancestors 'self';".
+                " form-action 'self';"
+            );
+        }
+
+        public function javascripts_nonce(
+            $nonce
+        ): string {
+            return $this->nonce;
+        }
+
+        public function stylesheets_nonce(
+            $nonce
+        ): string {
+            return $this->nonce;
+        }
+    }
---
+++ modules/content_security_policy/info.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    "name"          => __("Content Security Policy", "content_security_policy"),
+    "url"           => "http://chyrplite.net/",
+    "version"       => "2026.01",
+    "description"   => __("Adds a strict <code>Content-Security-Policy</code> header to the blog.", "content_security_policy"),
+    "author"        => array(
+        "name"      => "Daniel Pimley",
+        "url"       => "http://pimley.net/"
+    )
+);
