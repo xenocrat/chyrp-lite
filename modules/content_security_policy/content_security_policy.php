fc1d500c7660fae0259e397cebd147702337eeab
Daniel Pimley <dan@pimley.net>
2026-03-04 08:49:49 +0000

    CSP module: remove nonce support

--- modules/content_security_policy/content_security_policy.php
+++ modules/content_security_policy/content_security_policy.php
@@ -1,42 +1,18 @@
 <?php
     class ContentSecurityPolicy extends Modules {
-        private $nonce;
-        private $hash;
-
         public function runtime(
         ): void {
-            $this->nonce = base64_encode(
-                random(32)
-            );
-
-            $this->hash = base64_encode(
+            $error_style_hash = base64_encode(
                 hash("sha256", error_style(), true)
             );
 
             header(
                 "Content-Security-Policy:".
                 " default-src 'self';".
-                " style-src 'self' 'nonce-".$this->nonce."' 'sha256-".$this->hash."';".
-                " script-src 'self' 'nonce-".$this->nonce."';".
+                " style-src 'self' 'sha256-".$error_style_hash."';".
+                " script-src 'self';".
                 " frame-ancestors 'self';".
                 " form-action 'self';"
             );
-        }
-
-        public function javascripts_nonce(
-            $nonce
-        ): string {
-            return $this->nonce;
-        }
-
-        public function stylesheets_nonce(
-            $nonce
-        ): string {
-            return $this->nonce;
-        }
-
-        public function not_public_cacheable(
-        ): bool {
-            return true;
         }
     }
