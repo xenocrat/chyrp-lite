c841c6c0b61a985076de7e981d9d6df47bc3e438
Daniel Pimley <dan@pimley.net>
2026-06-16 20:10:12 +0100

    Feathers: bring filtering inside the class

--- includes/class/Feathers.php
+++ includes/class/Feathers.php
@@ -26,93 +26,75 @@
 
         # Array: $filters
         # Manages named trigger filters for feather fields.
-        public static $filters = array();
+        private static $filters = array();
 
         # Array: $custom_filters
         # Manages custom feather-provided trigger filters.
-        public static $custom_filters = array();
+        private static $custom_filters = array();
 
         /**
-         * Function: setFilter
-         * Applies a filter to a specified field of the Feather.
+         * Function: filter
+         * Applies feather filters to a post.
          *
          * Parameters:
-         *     $field - Attribute of the post to filter.
-         *     $name - Name of the filter to use.
+         *     $post - The post object to filter.
          *
          * See Also:
-         *     <Trigger.filter>
+         *     <Post.filter>
+         *     <Feathers.filterField>
          */
-        protected function setFilter(
-            $field,
-            $name
-        ): void {
-            self::$filters[get_class($this)][] = array(
-                "field" => $field,
-                "name" => $name
-            );
+        public static function filter(
+            $post
+        ): Post|false {
+            if ($post->no_results)
+                return false;
 
-            if (isset($this->fields[$field])) {
-                foreach ((array) $name as $filter)
-                    $this->fields[$field]["filters"][] = $filter;
+            $trigger = Trigger::current();
+            $touched = array();
+            $class_name = camelize($post->feather);
+
+            # Custom filters.
+            if (isset(self::$custom_filters[$class_name])) {
+                foreach (self::$custom_filters[$class_name] as $custom_filter) {
+                    $field = $custom_filter["field"];
+                    $field_unfiltered = $field."_unfiltered";
+
+                    if (!in_array($field_unfiltered, $touched)) {
+                        $post->$field_unfiltered = $post->$field ?? null ;
+                        $touched[] = $field_unfiltered;
+                    }
+
+                    $post->$field = call_user_func_array(
+                        array(
+                            self::$instances[$post->feather],
+                            $custom_filter["name"]
+                        ),
+                        array($post->$field, $post)
+                    );
+                }
             }
-        }
 
-        /**
-         * Function: customFilter
-         * Allows a Feather to apply its own filter to a specified field.
-         *
-         * Parameters:
-         *     $field - Attribute of the post to filter.
-         *     $name - Name of the class function to use as the filter.
-         *
-         * See Also:
-         *     <Trigger.filter>
-         */
-        protected function customFilter(
-            $field,
-            $name
-        ): void {
-            self::$custom_filters[get_class($this)][] = array(
-                "field" => $field,
-                "name" => $name
-            );
+            # Trigger filters.
+            if (isset(self::$filters[$class_name])) {
+                foreach (self::$filters[$class_name] as $filter) {
+                    $field = $filter["field"];
+                    $field_unfiltered = $field."_unfiltered";
 
-            if (isset($this->fields[$field])) {
-                foreach ((array) $name as $filter)
-                    $this->fields[$field]["custom_filters"][] = $filter;
+                    if (!in_array($field_unfiltered, $touched)) {
+                        $post->$field_unfiltered = $post->$field ?? null ;
+                        $touched[] = $field_unfiltered;
+                    }
+
+                    $trigger->filter($post->$field, $filter["name"], $post);
+                }
             }
-        }
 
-        /**
-         * Function: respondTo
-         * Allows a Feather to respond to a trigger as a module would.
-         *
-         * Parameters:
-         *     $name - Name of the trigger to respond to.
-         *     $function - Name of the class function to respond with.
-         *     $priority - Priority of the response.
-         *
-         * See Also:
-         *     <Trigger>
-         */
-        protected function respondTo(
-            $name,
-            $function = null,
-            $priority = 10
-        ): void {
-            fallback($function, $name);
-
-            Trigger::current()->add(
-                $name,
-                $priority,
-                array($this, $function)
-            );
+            return $post;
         }
 
         /**
          * Function: setField
-         * Sets the feather's fields for creating/editing posts with that feather.
+         * Defines a feather field to be used for post writing and editing.
          *
          * Parameters:
          *     $options - An array of key => val options for the field.
@@ -129,5 +111,98 @@
             $options
         ): void {
             $this->fields[$options["attr"]] = $options;
+        }
+
+        /**
+         * Function: setFilter
+         * Applies a filter to a specified field of the feather.
+         *
+         * Parameters:
+         *     $field - Attribute of the post to filter.
+         *     $name - Name of the filter to use.
+         *
+         * Returns:
+         *     @true@ or @false@
+         *
+         * See Also:
+         *     <Trigger.filter>
+         */
+        protected function setFilter(
+            $field,
+            $name
+        ): bool {
+            if (!isset($this->fields[$field]))
+                return false;
+
+            self::$filters[get_class($this)][] = array(
+                "field" => $field,
+                "name" => $name
+            );
+
+            foreach ((array) $name as $filter)
+                $this->fields[$field]["filters"][] = $filter;
+
+            return true;
+        }
+
+        /**
+         * Function: customFilter
+         * Allows a Feather to apply its own filter to a specified field.
+         *
+         * Parameters:
+         *     $field - Attribute of the post to filter.
+         *     $name - Name of the class function to use as the filter.
+         *
+         * Returns:
+         *     @true@ or @false@
+         *
+         * See Also:
+         *     <Trigger.filter>
+         */
+        protected function customFilter(
+            $field,
+            $name
+        ): bool {
+            if (!isset($this->fields[$field]))
+                return false;
+
+            self::$custom_filters[get_class($this)][] = array(
+                "field" => $field,
+                "name" => $name
+            );
+
+            foreach ((array) $name as $filter)
+                $this->fields[$field]["custom_filters"][] = $filter;
+
+            return true;
+        }
+
+        /**
+         * Function: respondTo
+         * Allows a Feather to respond to a trigger as a module would.
+         *
+         * Parameters:
+         *     $name - Name of the trigger to respond to.
+         *     $function - Name of the class function to respond with.
+         *     $priority - Priority of the response.
+         *
+         * Returns:
+         *     @true@ or @false@
+         *
+         * See Also:
+         *     <Trigger>
+         */
+        protected function respondTo(
+            $name,
+            $function = null,
+            $priority = 10
+        ): bool {
+            fallback($function, $name);
+
+            return Trigger::current()->add(
+                $name,
+                $priority,
+                array($this, $function)
+            );
         }
     }
