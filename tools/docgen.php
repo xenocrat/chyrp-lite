3f5e0b1e4295496bfaa8fab616deeb655c7e5bf4
Daniel Pimley <dan@pimley.net>
2026-08-07 20:34:45 +0100

    Bump version

--- tools/docgen.php
+++ tools/docgen.php
@@ -7,7 +7,7 @@
     header("Content-Type: text/html; charset=UTF-8");
 
     define('DEBUG',                         true);
-    define('CHYRP_VERSION',                 "2026.02.01");
+    define('CHYRP_VERSION',                 "2026.02.02");
     define('CHYRP_CODENAME',                "Caspian");
     define('CHYRP_IDENTITY',                "Chyrp/".CHYRP_VERSION." (".CHYRP_CODENAME.")");
     define('CHYRP_ENVIRONMENT',             false);
